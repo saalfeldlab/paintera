2b5d5c5610fce627065ebd4d73e38b46b89feea3
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-04-26 11:39:25 -0400

    implemented LabelFillController with imglib-algorithm FloodFill

--- src/main/java/bdv/bigcat/control/LabelFillController.java
+++ src/main/java/bdv/bigcat/control/LabelFillController.java
@@ -1,26 +1,24 @@
 package bdv.bigcat.control;
 
-import java.awt.Cursor;
-
-import org.scijava.ui.behaviour.Behaviour;
-import org.scijava.ui.behaviour.BehaviourMap;
-import org.scijava.ui.behaviour.ClickBehaviour;
-import org.scijava.ui.behaviour.InputTriggerAdder;
-import org.scijava.ui.behaviour.InputTriggerMap;
-import org.scijava.ui.behaviour.io.InputTriggerConfig;
-
 import bdv.bigcat.FragmentSegmentAssignment;
 import bdv.labels.labelset.Label;
-import bdv.labels.labelset.LabelMultisetFill;
 import bdv.labels.labelset.LabelMultisetType;
+import bdv.labels.labelset.Multiset;
 import bdv.viewer.ViewerPanel;
+import net.imglib2.*;
 import net.imglib2.Point;
-import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.RealPoint;
+import net.imglib2.algorithm.fill.Filter;
+import net.imglib2.algorithm.fill.FloodFill;
+import net.imglib2.algorithm.neighborhood.DiamondShape;
 import net.imglib2.algorithm.neighborhood.Shape;
 import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.type.numeric.integer.LongType;
+import net.imglib2.util.Pair;
 import net.imglib2.view.Views;
+import org.scijava.ui.behaviour.*;
+import org.scijava.ui.behaviour.io.InputTriggerConfig;
+
+import java.awt.Cursor;
 
 /**
  *
@@ -127,20 +125,31 @@ public class LabelFillController
 				viewer.setCursor( Cursor.getPredefinedCursor( Cursor.WAIT_CURSOR ) );
 				setCoordinates( x, y );
 				System.out.println( "Filling " + labelLocation + " with " + selectionController.getActiveFragmentId() );
+
 				final Point p = new Point(
-						( long )Math.round( labelLocation.getDoublePosition( 0 ) ),
-						( long )Math.round( labelLocation.getDoublePosition( 1 ) ),
-						( long )Math.round( labelLocation.getDoublePosition( 2 ) ) );
+						Math.round( labelLocation.getDoublePosition( 0 ) ),
+						Math.round( labelLocation.getDoublePosition( 1 ) ),
+						Math.round( labelLocation.getDoublePosition( 2 ) )
+				);
+
+				RandomAccess<LongType> paintAccess = paintedLabels.randomAccess();
+				paintAccess.setPosition( p );
+				long seedPaint = paintAccess.get().getIntegerLong();
+				long seedFragmentLabel = getBiggestLabel( labels, p );
+
 				final long t0 = System.currentTimeMillis();
-				LabelMultisetFill.fill(
+				FloodFill.fill(
 						Views.extendValue( labels, new LabelMultisetType() ),
 						Views.extendValue( paintedLabels, new LongType( Label.TRANSPARENT ) ),
 						p,
-						shape,
-						new LabelMultisetFill.IntegerTypeFillPolicySegmentsConsiderBackgroundAndCanvas2.Factory<>(
-								selectionController.getActiveFragmentId(),
-								assignment
-						) );
+						new LabelMultisetType(),
+						new LongType( selectionController.getActiveFragmentId() ),
+						new DiamondShape( 1 ),
+						new SegmentAndPaintFilter1(
+								seedPaint,
+								seedFragmentLabel,
+								assignment )
+				);
 				final long t1 = System.currentTimeMillis();
 				System.out.println( "Filling took " + (t1-t0) + " ms" );
 				viewer.setCursor( Cursor.getPredefinedCursor( Cursor.DEFAULT_CURSOR ) );
@@ -148,4 +157,78 @@ public class LabelFillController
 			}
 		}
 	}
+
+
+	public static class SegmentAndPaintFilter1 implements Filter< Pair< LabelMultisetType, LongType >, Pair< LabelMultisetType, LongType > >
+	{
+		private final long comparison;
+		private final long[] fragmentsContainedInSeedSegment;
+
+		public SegmentAndPaintFilter1( long seedPaint, long seedFragmentLabel, FragmentSegmentAssignment assignment ) {
+			this.comparison = seedPaint == Label.TRANSPARENT ? seedFragmentLabel : seedPaint;
+			this.fragmentsContainedInSeedSegment = assignment.getFragments( assignment.getSegment( comparison ) );
+		}
+
+		@Override
+		public boolean accept( Pair<LabelMultisetType, LongType> current, Pair<LabelMultisetType, LongType> reference ) {
+
+			final LabelMultisetType currentLabelSet = current.getA();
+			final long currentPaint = current.getB().getIntegerLong();
+
+			if ( currentPaint != Label.TRANSPARENT )
+				return currentPaint == comparison && currentPaint != reference.getB().getIntegerLong();
+
+			else {
+				for ( long fragment : this.fragmentsContainedInSeedSegment ) {
+					if ( currentLabelSet.contains( fragment ) )
+						return true;
+				}
+			}
+
+			return false;
+		}
+	}
+
+
+	public static class FragmentFilter implements Filter< Pair< LabelMultisetType, LongType >, Pair< LabelMultisetType, LongType > >
+	{
+
+		private final long seedLabel;
+
+		public FragmentFilter(long seedLabel) {
+			this.seedLabel = seedLabel;
+		}
+
+		@Override
+		public boolean accept( Pair< LabelMultisetType, LongType > current, Pair< LabelMultisetType, LongType > reference ) {
+			return ( current.getB().getIntegerLong() != reference.getB().getIntegerLong() ) && ( current.getA().contains( seedLabel ) );
+		}
+
+	}
+
+
+	public static long getBiggestLabel( RandomAccessible< LabelMultisetType > accessible, Localizable position )
+	{
+		RandomAccess<LabelMultisetType> access = accessible.randomAccess();
+		access.setPosition( position );
+		return getBiggestLabel( access.get() );
+	}
+
+
+	public static long getBiggestLabel( LabelMultisetType t )
+	{
+		int  maxCount = Integer.MIN_VALUE;
+		long maxLabel = -1;
+		for ( Multiset.Entry<Label> e : t.entrySet())
+		{
+			int c = e.getCount();
+			if ( c > maxCount )
+			{
+				maxLabel = e.getElement().id();
+				maxCount = c;
+			}
+		}
+		return maxLabel;
+	}
+
 }
