b9a3884ea3b4f3d62a871da7259f100eb7c7e2c0
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-05-09 15:02:38 -0400

    fixed imports in BigCATAriadne

--- src/main/java/bdv/bigcat/BigCATAriadne.java
+++ src/main/java/bdv/bigcat/BigCATAriadne.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import bdv.bigcat.control.*;
 import org.scijava.ui.behaviour.io.InputTriggerConfig;
 
 import com.google.gson.JsonIOException;
@@ -13,15 +14,6 @@ import bdv.BigDataViewer;
 import bdv.bigcat.composite.ARGBCompositeAlphaYCbCr;
 import bdv.bigcat.composite.Composite;
 import bdv.bigcat.composite.CompositeCopy;
-import bdv.bigcat.control.LabelBrushController;
-import bdv.bigcat.control.LabelFillController;
-import bdv.bigcat.control.LabelMultiSetIdPicker;
-import bdv.bigcat.control.LabelPersistenceController;
-import bdv.bigcat.control.LabelRestrictToSegmentController;
-import bdv.bigcat.control.MergeController;
-import bdv.bigcat.control.PairLabelMultiSetLongIdPicker;
-import bdv.bigcat.control.SelectionController;
-import bdv.bigcat.control.TranslateZController;
 import bdv.bigcat.ui.ARGBConvertedLabelPairSource;
 import bdv.bigcat.ui.GoldenAngleSaturatedARGBStream;
 import bdv.bigcat.ui.Util;
