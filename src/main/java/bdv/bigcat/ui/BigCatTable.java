1021f71e0f28b4fa6320bbbbf5a1f211a037b640
Jan Funke <jfunke@iri.upc.edu>
2016-05-09 16:46:25 +0200

    BigCatTable: remove table's keybindings during cell editing
    This avoids that when an ordinary key like "g" is mapped to an action,
    subsequent typing of "g" during a cell edit fires the action.

--- src/main/java/bdv/bigcat/ui/BigCatTable.java
+++ src/main/java/bdv/bigcat/ui/BigCatTable.java
@@ -2,14 +2,25 @@ package bdv.bigcat.ui;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.InputMap;
+import javax.swing.JComponent;
 import javax.swing.JTable;
+import javax.swing.KeyStroke;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
+import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableModel;
@@ -24,16 +35,15 @@ public class BigCatTable extends JTable implements TableModelListener {
 
 	private static final int MIN_COLUMN_WIDTH = 50;
 	private static final double ENLARGE_COLUMN_WIDTH = 1.05;
-	
+
 	private boolean inLayout = false;
 	private boolean initialized = false;
-	
+
 	// don't change column widths that the user set manually
 	private final Set<Integer> manuallyChangedColumuns = new HashSet<Integer>();
-	
+
 	public BigCatTable(TableModel tm) {
 		super(tm);
-		setSurrendersFocusOnKeystroke(true);
 		setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		setIntercellSpacing(new Dimension(2, 2));
 		getModel().addTableModelListener(this);
@@ -98,11 +108,48 @@ public class BigCatTable extends JTable implements TableModelListener {
 			setPreferredColumnWidths();
 	}
 
-    protected boolean hasExcessWidth() {
-    	
-        return getPreferredSize().width < getParent().getWidth();
-    }
-    
+	@Override
+	public Component prepareEditor(TableCellEditor editor, int row, int column) {
+
+		JComponent editorComponent = (JComponent) super.prepareEditor(editor, row, column);
+
+		// Get all keystrokes that are mapped to actions in higher components
+		List<KeyStroke> allTableKeys = new LinkedList<KeyStroke>();
+		for (Container c = this; c != null; c = c.getParent()) {
+			if (c instanceof JComponent) {
+				InputMap inputMap = ((JComponent)c).getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+				KeyStroke[] tableKeys = inputMap.allKeys();
+				if (tableKeys != null)
+					allTableKeys.addAll(Arrays.asList(tableKeys));
+
+			}
+		}
+
+		// An action that does nothing. We can not just map to "none",
+		// as this is not interrupting the action-name -> action search.
+		// We have to map to a proper action, "nothing" in this case.
+		Action nada = new AbstractAction() {
+			private static final long serialVersionUID = 1L;
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+		};
+
+		// Replace every table key binding with nothing, thus creating an
+		// event-barrier.
+		for (KeyStroke key : allTableKeys) {
+			editorComponent.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(key,	"nothing");
+			editorComponent.getActionMap().put("nothing", nada);
+		}
+
+		return editorComponent;
+	}
+
+	protected boolean hasExcessWidth() {
+
+		return getPreferredSize().width < getParent().getWidth();
+	}
+
 	protected void setPreferredColumnWidths() {
 
 		for (int column = 0; column < getColumnCount(); column++) {
