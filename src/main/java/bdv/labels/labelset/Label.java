522519a8ba3a5cd08b296aa9b036d567b3f4a849
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2016-09-12 16:51:55 -0400

    added BACKGROUND label (=0) which is considered a 'regular' label

--- src/main/java/bdv/labels/labelset/Label.java
+++ src/main/java/bdv/labels/labelset/Label.java
@@ -4,6 +4,8 @@ import bdv.util.IdService;
 
 public interface Label
 {
+	static public long BACKGROUND = 0L;
+
 	static public long TRANSPARENT = 0xffffffffffffffffL; // -1L or uint64.MAX_VALUE
 	static public long INVALID = 0xfffffffffffffffeL; // -2L or uint64.MAX_VALUE - 1
 	static public long OUTSIDE = 0xfffffffffffffffdL; // -3L or uint64.MAX_VALUE - 2
