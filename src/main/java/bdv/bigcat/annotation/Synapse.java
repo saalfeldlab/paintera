9987e5a338f775f4283572c5ec60e29c9fe184cb
Vanessa Leite <vrcleite@gmail.com>
2017-05-05 16:44:01 +0200

    Remove get/set methods that are unnecessary

--- src/main/java/bdv/bigcat/annotation/Synapse.java
+++ src/main/java/bdv/bigcat/annotation/Synapse.java
@@ -1,8 +1,5 @@
 package bdv.bigcat.annotation;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import net.imglib2.RealPoint;
 
 public class Synapse extends Annotation {
@@ -11,33 +8,9 @@ public class Synapse extends Annotation {
 		super(id, pos, comment);
 	}
 
-	public PostSynapticSite getPreSynapticPartner() {
-		return preSynapticPartner;
-	}
-
-	public void setPreSynapticPartner(PostSynapticSite preSynapticPartner) {
-		this.preSynapticPartner = preSynapticPartner;
-	}
-
-	public List<PostSynapticSite> getPostSynapticPartners() {
-		return postSynapticPartners;
-	}
-
-	public void setPostSynapticPartners(List<PostSynapticSite> postSynapticPartners) {
-		this.postSynapticPartners = postSynapticPartners;
-	}
-
-	public void addPostSynapticPartner(PostSynapticSite site) {
-		this.postSynapticPartners.add(site);
-		
-	}
-
 	@Override
 	public void accept(AnnotationVisitor visitor) {
 		super.accept(visitor);
 		visitor.visit(this);
-	}
-	
-	private PostSynapticSite preSynapticPartner;
-	private List< PostSynapticSite > postSynapticPartners = new LinkedList< PostSynapticSite >();
+	}	
 }
