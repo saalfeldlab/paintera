2f8a0fe71305c1c8fa9ff1bfbe3a4bd8e860b05e
Vanessa Leite <vrcleite@gmail.com>
2017-07-21 09:22:27 +0200

    Add Chunk
    structure that contains the volume and its offset

---
+++ src/main/java/bdv/bigcat/viewer/Chunk.java
@@ -0,0 +1,37 @@
+package bdv.bigcat.viewer;
+
+import bdv.labels.labelset.LabelMultisetType;
+import net.imglib2.RandomAccessibleInterval;
+
+public class Chunk
+{
+	private RandomAccessibleInterval< LabelMultisetType > volume;
+	private int[] offset;
+	
+	public Chunk()
+	{
+		volume = null;
+		offset = null;
+	}
+
+	public RandomAccessibleInterval< LabelMultisetType > getVolume()
+	{
+		return volume;
+	}
+
+	public void setVolume( RandomAccessibleInterval< LabelMultisetType > volume )
+	{
+		this.volume = volume;
+	}
+
+	public int[] getOffset()
+	{
+		return offset;
+	}
+
+	public void setOffset( int[] offset )
+	{
+		this.offset = offset;
+	}
+
+}
