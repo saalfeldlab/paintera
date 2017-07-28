149bf63362f6eb485d09c7b7b8054c0df49ad7e1
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-07-28 14:52:02 -0400

    Add example application for Pape

---
+++ src/main/java/bdv/bigcat/viewer/ExampleApplicationPape.java
@@ -0,0 +1,192 @@
+package bdv.bigcat.viewer;
+
+import java.util.Arrays;
+
+import com.sun.javafx.application.PlatformImpl;
+
+import bdv.AbstractViewerSetupImgLoader;
+import bdv.viewer.Interpolation;
+import bdv.viewer.Source;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import mpicbg.spim.data.sequence.VoxelDimensions;
+import net.imglib2.FinalInterval;
+import net.imglib2.RandomAccessible;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.RealRandomAccessible;
+import net.imglib2.Volatile;
+import net.imglib2.converter.Converter;
+import net.imglib2.converter.Converters;
+import net.imglib2.display.AbstractLinearRange;
+import net.imglib2.interpolation.InterpolatorFactory;
+import net.imglib2.interpolation.randomaccess.ClampingNLinearInterpolatorFactory;
+import net.imglib2.interpolation.randomaccess.NearestNeighborInterpolatorFactory;
+import net.imglib2.realtransform.AffineTransform3D;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.volatiles.VolatileARGBType;
+import net.imglib2.util.Intervals;
+import net.imglib2.view.ExtendedRandomAccessibleInterval;
+import net.imglib2.view.Views;
+
+public class ExampleApplicationPape
+{
+
+	public static void main( final String[] args ) throws Exception
+	{
+
+		PlatformImpl.startup( () -> {} );
+
+		final String rawFile = "/groups/saalfeld/home/hanslovskyp/from_papec/sample_D+/sampleD+_raw_downscaled64.h5";
+		final String rawDataset = "data";
+
+//		final double[] resolution = { 8 * 4, 8 * 4, 8 };
+		final double[] resolution = { 1, 1, 1 };
+		final int[] cellSize = { 1024, 1024, 1 };
+
+		final HDF5UnsignedByteSpec rawSource = new HDF5UnsignedByteSpec( rawFile, rawDataset, cellSize, resolution, 0, 255 );
+
+		final double[] min = Arrays.stream( Intervals.minAsLongArray( rawSource.getSource().getSource( 0, 0 ) ) ).mapToDouble( v -> v ).toArray();
+		final double[] max = Arrays.stream( Intervals.maxAsLongArray( rawSource.getSource().getSource( 0, 0 ) ) ).mapToDouble( v -> v ).toArray();
+		final AffineTransform3D affine = new AffineTransform3D();
+		rawSource.getSource().getSourceTransform( 0, 0, affine );
+		affine.apply( min, min );
+		affine.apply( max, max );
+
+		final Atlas viewer = new Atlas( new FinalInterval( Arrays.stream( min ).mapToLong( Math::round ).toArray(), Arrays.stream( max ).mapToLong( Math::round ).toArray() ) );
+
+		Platform.runLater( () -> {
+			final Stage stage = new Stage();
+			viewer.start( stage );
+			stage.show();
+		} );
+
+		viewer.addSource( rawSource );
+
+	}
+
+	public static class VolatileRealARGBConverter< T extends RealType< T > > extends AbstractLinearRange implements Converter< Volatile< T >, VolatileARGBType >
+	{
+
+		public VolatileRealARGBConverter( final double min, final double max )
+		{
+			super( min, max );
+		}
+
+		@Override
+		public void convert( final Volatile< T > input, final VolatileARGBType output )
+		{
+			final boolean isValid = input.isValid();
+			output.setValid( isValid );
+			if ( isValid )
+			{
+				final double a = input.get().getRealDouble();
+				final int b = Math.min( 255, roundPositive( Math.max( 0, ( a - min ) / scale * 255.0 ) ) );
+				final int argb = 0xff000000 | ( b << 8 | b ) << 8 | b;
+				output.set( argb );
+			}
+		}
+
+	}
+
+	public static class ARGBConvertedSource< T > implements Source< VolatileARGBType >
+	{
+		final private AbstractViewerSetupImgLoader< T, ? extends Volatile< T > > loader;
+
+		private final int setupId;
+
+		private final Converter< Volatile< T >, VolatileARGBType > converter;
+
+		final protected InterpolatorFactory< VolatileARGBType, RandomAccessible< VolatileARGBType > >[] interpolatorFactories;
+		{
+			interpolatorFactories = new InterpolatorFactory[] {
+					new NearestNeighborInterpolatorFactory< VolatileARGBType >(),
+					new ClampingNLinearInterpolatorFactory< VolatileARGBType >()
+			};
+		}
+
+		public ARGBConvertedSource(
+				final int setupId,
+				final AbstractViewerSetupImgLoader< T, ? extends Volatile< T > > loader,
+				final Converter< Volatile< T >, VolatileARGBType > converter )
+		{
+			this.setupId = setupId;
+			this.loader = loader;
+			this.converter = converter;
+		}
+
+		final public AbstractViewerSetupImgLoader< T, ? extends Volatile< T > > getLoader()
+		{
+			return loader;
+		}
+
+		@Override
+		public RandomAccessibleInterval< VolatileARGBType > getSource( final int t, final int level )
+		{
+			return Converters.convert(
+					loader.getVolatileImage( t, level ),
+					converter,
+					new VolatileARGBType() );
+		}
+
+		@Override
+		public void getSourceTransform( final int t, final int level, final AffineTransform3D transform )
+		{
+			transform.set( loader.getMipmapTransforms()[ level ] );
+		}
+
+		/**
+		 * TODO Store this in a field
+		 */
+		@Override
+		public int getNumMipmapLevels()
+		{
+			return loader.getMipmapResolutions().length;
+		}
+
+		@Override
+		public boolean isPresent( final int t )
+		{
+			return t == 0;
+		}
+
+		@Override
+		public RealRandomAccessible< VolatileARGBType > getInterpolatedSource( final int t, final int level, final Interpolation method )
+		{
+
+			final ExtendedRandomAccessibleInterval< VolatileARGBType, RandomAccessibleInterval< VolatileARGBType > > extendedSource =
+					Views.extendValue( getSource( t, level ), new VolatileARGBType( 0 ) );
+			switch ( method )
+			{
+			case NLINEAR:
+				return Views.interpolate( extendedSource, interpolatorFactories[ 1 ] );
+			default:
+				return Views.interpolate( extendedSource, interpolatorFactories[ 0 ] );
+			}
+		}
+
+		@Override
+		public VolatileARGBType getType()
+		{
+			return new VolatileARGBType();
+		}
+
+		@Override
+		public String getName()
+		{
+			return "1 2 3";
+		}
+
+		@Override
+		public VoxelDimensions getVoxelDimensions()
+		{
+			return null;
+		}
+
+		// TODO: make ARGBType version of this source
+		public Source nonVolatile()
+		{
+			return this;
+		}
+	}
+
+}
