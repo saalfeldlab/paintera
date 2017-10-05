c0d6b685518c101a8a01eabd55e8c337e7f123d8
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-10-05 15:56:06 -0400

    Remove obsolete method

--- src/main/java/bdv/bigcat/viewer/viewer3d/util/Chunk.java
+++ src/main/java/bdv/bigcat/viewer/viewer3d/util/Chunk.java
@@ -1,7 +1,6 @@
 package bdv.bigcat.viewer.viewer3d.util;
 
 import net.imglib2.Interval;
-import net.imglib2.Localizable;
 
 /**
  * Chunk is a part of the volume. Each chunk knows its offset and keeps its mesh
@@ -38,25 +37,5 @@ public class Chunk< T >
 	public Interval interval()
 	{
 		return this.interval;
-	}
-
-	/**
-	 * Indicates if the chunk contains the position informed. The bounding box
-	 * of the chunk takes into account its offset.
-	 *
-	 * @param position
-	 *            x, y, z in world coordinates
-	 * @return true if the point is inside the chunk, false otherwise.
-	 */
-	public boolean contains( final Localizable location )
-	{
-		assert interval.numDimensions() == location.numDimensions(): "volume dimension is " + interval.numDimensions() +
-				" and point dimension is " + location.numDimensions();
-
-		for ( int i = 0; i < interval.numDimensions(); i++ )
-			if ( interval.max( i ) < location.getIntPosition( i ) || interval.min( i ) > location.getIntPosition( i ) )
-				return false;
-
-		return true;
 	}
 }
