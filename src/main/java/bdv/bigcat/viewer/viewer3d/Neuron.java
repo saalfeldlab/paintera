6213257ddae777dd20fd064dbdd4ea3f0efb5d14
Vanessa Leite <vrcleite@gmail.com>
2017-10-18 09:58:48 +0200

    Add boxes representing the chunks to be processed
    Still need to make them kind of 'pulsating' or at least transparent

--- src/main/java/bdv/bigcat/viewer/viewer3d/Neuron.java
+++ src/main/java/bdv/bigcat/viewer/viewer3d/Neuron.java
@@ -13,6 +13,7 @@ import bdv.bigcat.viewer.viewer3d.marchingCubes.ForegroundCheck;
 import bdv.bigcat.viewer.viewer3d.marchingCubes.MarchingCubes;
 import bdv.bigcat.viewer.viewer3d.util.HashWrapper;
 import cleargl.GLVector;
+import graphics.scenery.Box;
 import graphics.scenery.Material;
 import graphics.scenery.Mesh;
 import graphics.scenery.Node;
@@ -22,6 +23,7 @@ import net.imglib2.Interval;
 import net.imglib2.Localizable;
 import net.imglib2.Point;
 import net.imglib2.RandomAccessible;
+import net.imglib2.RealPoint;
 import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.util.Intervals;
 
@@ -110,6 +112,33 @@ public class Neuron< T >
 					final Interval interval = new FinalInterval(
 							coordinates,
 							IntStream.range( 0, coordinates.length ).mapToLong( d -> coordinates[ d ] + blockSize[ d ] ).toArray() );
+
+					final RealPoint p = new RealPoint( interval.numDimensions() );
+
+					// TODO: get the resolution from the data
+					p.setPosition( interval.dimension( 0 ) * 4, 0 );
+					p.setPosition( interval.dimension( 1 ) * 4, 1 );
+					p.setPosition( interval.dimension( 2 ) * 40, 2 );
+
+					final Box chunk = new Box( new GLVector( p.getFloatPosition( 0 ), p.getFloatPosition( 1 ), p.getFloatPosition( 2 ) ), true );
+					System.out.println( "box size: " + p.getFloatPosition( 0 ) + " " + p.getFloatPosition( 1 ) + " " + p.getFloatPosition( 2 ) );
+
+					p.setPosition( coordinates[ 0 ], 0 );
+					p.setPosition( coordinates[ 1 ], 1 );
+					p.setPosition( coordinates[ 2 ], 2 );
+					toWorldCoordinates.apply( p, p );
+					System.out.println( "coordinates: " + p.getFloatPosition( 0 ) + " " + p.getFloatPosition( 1 ) + " " + p.getFloatPosition( 2 ) );
+					chunk.setPosition( new GLVector( p.getFloatPosition( 0 ), p.getFloatPosition( 1 ), p.getFloatPosition( 2 ) ) );
+
+					final GLVector colorVector = new GLVector( ( color >>> 16 & 0xff ) * ONE_OVER_255, ( color >>> 8 & 0xff ) * ONE_OVER_255, ( color >>> 0 & 0xff ) * ONE_OVER_255 );
+					chunk.getMaterial().setDiffuse( colorVector );
+
+					// TODO: this is not working properly... the transparency
+					// and the opacity are not combined
+					chunk.getMaterial().setOpacity( 0.1f );
+//					chunk.getMaterial().setTransparent( true );
+					scene.addChild( chunk );
+
 					final MarchingCubes< T > mc = new MarchingCubes<>( data, interval, toWorldCoordinates, cubeSize );
 					final float[] vertices = mc.generateMesh( foregroundCheck );
 
@@ -122,9 +151,9 @@ public class Neuron< T >
 						final Mesh mesh = new Mesh();
 						final Material material = new Material();
 						material.setOpacity( 1.0f );
-						material.setDiffuse( new GLVector( ( color >>> 16 & 0xff ) * ONE_OVER_255, ( color >>> 8 & 0xff ) * ONE_OVER_255, ( color >>> 0 & 0xff ) * ONE_OVER_255 ) );
-						material.setAmbient( new GLVector( ( color >>> 16 & 0xff ) * ONE_OVER_255, ( color >>> 8 & 0xff ) * ONE_OVER_255, ( color >>> 0 & 0xff ) * ONE_OVER_255 ) );
-						material.setSpecular( new GLVector( ( color >>> 16 & 0xff ) * ONE_OVER_255, ( color >>> 8 & 0xff ) * ONE_OVER_255, ( color >>> 0 & 0xff ) * ONE_OVER_255 ) );
+						material.setDiffuse( colorVector );
+						material.setAmbient( colorVector );
+						material.setSpecular( colorVector );
 						mesh.setMaterial( material );
 						mesh.setPosition( new GLVector( 0.0f, 0.0f, 0.0f ) );
 						mesh.setVertices( FloatBuffer.wrap( vertices ) );
@@ -133,6 +162,7 @@ public class Neuron< T >
 						{
 							if ( !isCanceled )
 							{
+								scene.removeChild( chunk );
 								addNode( mesh );
 								mesh.setDirty( true );
 							}
@@ -147,8 +177,9 @@ public class Neuron< T >
 							otherGridCoordinates[ d ] -= 2;
 							submitForOffset( otherGridCoordinates.clone(), data, foregroundCheck, toWorldCoordinates, blockSize, cubeSize, color, es );
 						}
-
 					}
+					else
+						scene.removeChild( chunk );
 				} ) );
 		}
 
