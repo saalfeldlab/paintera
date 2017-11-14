0df517b6d6b18b5e666257592c9da607c60e2838
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2017-11-13 20:44:46 -0500

    add N5 simple and mipmap sources

--- src/main/java/bdv/bigcat/viewer/atlas/data/DataSource.java
+++ src/main/java/bdv/bigcat/viewer/atlas/data/DataSource.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.function.Supplier;
+
+import org.janelia.saalfeldlab.n5.N5Reader;
+import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
+
 import bdv.img.h5.H5Utils;
 import bdv.util.volatiles.SharedQueue;
 import bdv.util.volatiles.VolatileViews;
@@ -76,6 +80,157 @@ public interface DataSource< D, T > extends Source< T >
 						( RandomAccessibleInterval< V >[] ) new RandomAccessibleInterval[] {
 								VolatileViews.wrapAsVolatile( raw, sharedQueue, new CacheHints( LoadingStrategy.VOLATILE, priority, true ) ) },
 						new AffineTransform3D[] { rawTransform },
+						( interpolation ) -> {
+							switch ( ( Interpolation ) interpolation )
+							{
+							case NLINEAR:
+								return new NLinearInterpolatorFactory< T >();
+							default:
+								return new NearestNeighborInterpolatorFactory< T >();
+							}
+						},
+						( interpolation ) -> {
+							switch ( ( Interpolation ) interpolation )
+							{
+							case NLINEAR:
+								return new NLinearInterpolatorFactory< V >();
+							default:
+								return new NearestNeighborInterpolatorFactory< V >();
+							}
+						},
+						typeSupplier,
+						volatileTypeSupplier,
+						name );
+		return rawSource;
+	}
+
+	/**
+	 * Create a primitive single scale level source without visualization
+	 * conversion from an N5 dataset.
+	 *
+	 * @param name
+	 * @param n5
+	 * @param rawDataset
+	 * @param resolution
+	 * @param sharedQueue
+	 * @param priority
+	 * @param typeSupplier
+	 * @param volatileTypeSupplier
+	 * @return
+	 * @throws IOException
+	 */
+	public static < T extends NativeType< T > & NumericType< T >, V extends NumericType< V > > RandomAccessibleIntervalDataSource< T, V > createN5RawSource(
+			final String name,
+			final N5Reader n5,
+			final String dataset,
+			final double[] resolution,
+			final SharedQueue sharedQueue,
+			final int priority,
+			final Supplier< T > typeSupplier,
+			final Supplier< V > volatileTypeSupplier ) throws IOException
+	{
+		final RandomAccessibleInterval< T > raw = N5Utils.openVolatile( n5, dataset );
+
+		final AffineTransform3D rawTransform = new AffineTransform3D();
+		rawTransform.set(
+				resolution[ 0 ], 0, 0, 0,
+				0, resolution[ 1 ], 0, 0,
+				0, 0, resolution[ 2 ], 0 );
+
+		@SuppressWarnings( "unchecked" )
+		final RandomAccessibleIntervalDataSource< T, V > rawSource =
+				new RandomAccessibleIntervalDataSource< T, V >(
+						( RandomAccessibleInterval< T >[] ) new RandomAccessibleInterval[] { raw },
+						( RandomAccessibleInterval< V >[] ) new RandomAccessibleInterval[] {
+								VolatileViews.wrapAsVolatile( raw, sharedQueue, new CacheHints( LoadingStrategy.VOLATILE, priority, true ) ) },
+						new AffineTransform3D[] { rawTransform },
+						( interpolation ) -> {
+							switch ( ( Interpolation ) interpolation )
+							{
+							case NLINEAR:
+								return new NLinearInterpolatorFactory< T >();
+							default:
+								return new NearestNeighborInterpolatorFactory< T >();
+							}
+						},
+						( interpolation ) -> {
+							switch ( ( Interpolation ) interpolation )
+							{
+							case NLINEAR:
+								return new NLinearInterpolatorFactory< V >();
+							default:
+								return new NearestNeighborInterpolatorFactory< V >();
+							}
+						},
+						typeSupplier,
+						volatileTypeSupplier,
+						name );
+		return rawSource;
+	}
+
+	/**
+	 * Create a primitive multi scale level source without visualization
+	 * conversion from an N5 multi scale group.
+	 *
+	 * @param name
+	 * @param n5
+	 * @param group
+	 * @param resolution
+	 * @param sharedQueue
+	 * @param priority
+	 * @param typeSupplier
+	 * @param volatileTypeSupplier
+	 * @return
+	 * @throws IOException
+	 */
+	@SuppressWarnings( "unchecked" )
+	public static < T extends NativeType< T > & NumericType< T >, V extends NumericType< V > > RandomAccessibleIntervalDataSource< T, V > createN5MipmapRawSource(
+			final String name,
+			final N5Reader n5,
+			final String group,
+			final double[] resolution,
+			final SharedQueue sharedQueue,
+			final Supplier< T > typeSupplier,
+			final Supplier< V > volatileTypeSupplier ) throws IOException
+	{
+		final AffineTransform3D rawTransform = new AffineTransform3D();
+		rawTransform.set(
+				resolution[ 0 ], 0, 0, 0,
+				0, resolution[ 1 ], 0, 0,
+				0, 0, resolution[ 2 ], 0 );
+
+		final ArrayList< RandomAccessibleInterval< T > > mipmaps = new ArrayList<>();
+		final ArrayList< RandomAccessibleInterval< V > > volatileMipmaps = new ArrayList<>();
+		final ArrayList< AffineTransform3D > mipmapTransforms = new ArrayList<>();
+
+		final String[] sortedMipmapDatasets =
+				Arrays.stream( n5.list( group ) )
+				.filter( dataset -> dataset.matches( "s\\d+" ) )
+				.sorted()
+				.toArray( String[]::new );
+
+		for ( int i = 0; i < sortedMipmapDatasets.length; ++i )
+		{
+			final RandomAccessibleInterval< T > mipmap = N5Utils.openVolatile( n5, group + "/" + sortedMipmapDatasets[ i ] );
+			mipmaps.add( mipmap );
+			volatileMipmaps.add( ( RandomAccessibleInterval< V > )VolatileViews.wrapAsVolatile( mipmap, sharedQueue, new CacheHints( LoadingStrategy.VOLATILE, i, true ) ) );
+			final long[] downsampleFactors = n5.getAttribute( group + "/" + sortedMipmapDatasets[ i ], "downsamplingFactors", long[].class );
+			final AffineTransform3D mipmapTransform = rawTransform.copy();
+			if ( downsampleFactors != null )
+			{
+				mipmapTransform.set(
+						resolution[ 0 ] * downsampleFactors[ 0 ], 0, 0, 0.5 * ( downsampleFactors[ 0 ] - 1 ),
+						0, resolution[ 1 ] * downsampleFactors[ 1 ], 0, 0.5 * ( downsampleFactors[ 1 ] - 1 ),
+						0, 0, resolution[ 2 ] * downsampleFactors[ 2 ], 0.5 * ( downsampleFactors[ 2 ] - 1 ) );
+			}
+			mipmapTransforms.add( mipmapTransform );
+		}
+
+		final RandomAccessibleIntervalDataSource< T, V > rawSource =
+				new RandomAccessibleIntervalDataSource< T, V >(
+						mipmaps.toArray( new RandomAccessibleInterval[ 0 ] ),
+						volatileMipmaps.toArray( new RandomAccessibleInterval[ 0 ] ),
+						mipmapTransforms.toArray( new AffineTransform3D[ 0 ] ),
 						( interpolation ) -> {
 							switch ( ( Interpolation ) interpolation )
 							{
