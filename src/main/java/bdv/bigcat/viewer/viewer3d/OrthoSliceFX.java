e8a8ec43ac15809d15ac93c78387269e16dd9c24
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2017-11-15 16:45:23 -0500

    render orthoslices delayed and at max 512 x something pixels this avoids intermittent stopping during navigation and general slowness in 3D viewer while BDV panels are being updated
    NOTE We could not find a meaningful way in JavaFX 3D to update
    bitmap textures fast or to re-use existing textures from the scene
    PhongMaterial.setSelfIlluminationMap(Image) is unfortunately the only
    way to get a texture into a scene and, for unknown reasons has to be
    executed in the user thread which blocks everything else.  This may
    become the terminal reason to eventually leave JavaFX 3D and to seek
    for alternatives.

--- src/main/java/bdv/bigcat/viewer/viewer3d/OrthoSliceFX.java
+++ src/main/java/bdv/bigcat/viewer/viewer3d/OrthoSliceFX.java
@@ -13,7 +13,9 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.CullFace;
@@ -36,7 +38,7 @@ public class OrthoSliceFX
 	private final MeshView mv = new MeshView( mesh );
 
 	// no delay
-	LatestTaskExecutor es = new LatestTaskExecutor( 0 );
+	LatestTaskExecutor es = new LatestTaskExecutor( 50 * 1000 * 1000 );
 
 	private final PhongMaterial material;
 
@@ -105,12 +107,28 @@ public class OrthoSliceFX
 		}
 		if ( w <= 0 || h <= 0 )
 			return;
-//		viewerTransform.set( viewerTransform.get( 0, 3 ) - w / 2, 0, 3 );
-//		viewerTransform.set( viewerTransform.get( 1, 3 ) - h / 2, 1, 3 );
+		InvokeOnJavaFXApplicationThread.invoke( () -> {
+			mesh.update( new RealPoint( 0, 0 ), new RealPoint( w, 0 ), new RealPoint( w, h ), new RealPoint( 0, h ), viewerTransform.inverse() );
+		} );
 		es.execute( () -> {
+			final double scale = 512.0 / Math.max( w, h );
+			final int fitWidth = ( int )Math.round( w * scale );
+			final int fitHeight = ( int )Math.round( h * scale );
+			final ImageView imageView = new ImageView( image );
+			imageView.setPreserveRatio( true );
+			imageView.setFitWidth( fitWidth );
+			imageView.setFitHeight( fitHeight );
+			final SnapshotParameters snapshotParameters = new SnapshotParameters();
+			snapshotParameters.setFill( Color.BLACK );
 			InvokeOnJavaFXApplicationThread.invoke( () -> {
-				material.setSelfIlluminationMap( image );
-				mesh.update( new RealPoint( 0, 0 ), new RealPoint( w, 0 ), new RealPoint( w, h ), new RealPoint( 0, h ), viewerTransform.inverse() );
+				imageView.snapshot( snapshotResult -> {
+					InvokeOnJavaFXApplicationThread.invoke( () -> {
+
+						material.setSelfIlluminationMap( snapshotResult.getImage() );
+						mesh.update( new RealPoint( 0, 0 ), new RealPoint( w, 0 ), new RealPoint( w, h ), new RealPoint( 0, h ), viewerTransform.inverse() );
+					} );
+					return null;
+				}, snapshotParameters, null );
 			} );
 		} );
 	}
