62e4f1157151196e9fcf7f13bcd653fe00fcafcb
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2018-01-22 17:26:45 -0500

    bump n5 and n5-imglib2 versions

--- src/main/java/bdv/bigcat/viewer/ExampleApplicationSampleE.java
+++ src/main/java/bdv/bigcat/viewer/ExampleApplicationSampleE.java
@@ -4,14 +4,13 @@ import java.lang.invoke.MethodHandles;
 import java.util.Arrays;
 import java.util.concurrent.CountDownLatch;
 
-import org.janelia.saalfeldlab.n5.N5;
+import org.janelia.saalfeldlab.n5.N5FSReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.beust.jcommander.JCommander;
 import com.sun.javafx.application.PlatformImpl;
 
-import bdv.AbstractViewerSetupImgLoader;
 import bdv.bigcat.viewer.atlas.Atlas;
 import bdv.bigcat.viewer.atlas.data.DataSource;
 import bdv.bigcat.viewer.atlas.data.LabelDataSource;
@@ -21,28 +20,10 @@ import bdv.img.cache.VolatileGlobalCellCache;
 import bdv.util.IdService;
 import bdv.util.LocalIdService;
 import bdv.util.volatiles.SharedQueue;
-import bdv.viewer.Interpolation;
-import bdv.viewer.Source;
 import javafx.application.Platform;
 import javafx.stage.Stage;
-import mpicbg.spim.data.sequence.VoxelDimensions;
-import net.imglib2.RandomAccessible;
-import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.RealRandomAccessible;
-import net.imglib2.Volatile;
-import net.imglib2.converter.Converter;
-import net.imglib2.converter.Converters;
-import net.imglib2.display.AbstractLinearRange;
-import net.imglib2.interpolation.InterpolatorFactory;
-import net.imglib2.interpolation.randomaccess.ClampingNLinearInterpolatorFactory;
-import net.imglib2.interpolation.randomaccess.NearestNeighborInterpolatorFactory;
-import net.imglib2.realtransform.AffineTransform3D;
-import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
-import net.imglib2.type.volatiles.VolatileARGBType;
 import net.imglib2.type.volatiles.VolatileUnsignedByteType;
-import net.imglib2.view.ExtendedRandomAccessibleInterval;
-import net.imglib2.view.Views;
 
 public class ExampleApplicationSampleE
 {
@@ -82,10 +63,10 @@ public class ExampleApplicationSampleE
 		final VolatileGlobalCellCache cellCache = new VolatileGlobalCellCache( 1, 12 );
 
 		final RandomAccessibleIntervalDataSource< UnsignedByteType, VolatileUnsignedByteType > rawSource =
-				DataSource.createN5MipmapSource( "raw", N5.openFSReader( n5Path ), rawGroup, resolution, sharedQueue, UnsignedByteType::new, VolatileUnsignedByteType::new );
+				DataSource.createN5MipmapSource( "raw", new N5FSReader( n5Path ), rawGroup, resolution, sharedQueue, UnsignedByteType::new, VolatileUnsignedByteType::new );
 
 		final LabelDataSource labelSource =
-				LabelDataSource.createN5LabelSource( "labels", N5.openFSReader( labelsN5Path ), labelsDataset, resolution, sharedQueue, 0, new FragmentSegmentAssignmentOnlyLocal() );
+				LabelDataSource.createN5LabelSource( "labels", new N5FSReader( labelsN5Path ), labelsDataset, resolution, sharedQueue, 0, new FragmentSegmentAssignmentOnlyLocal() );
 
 		final IdService idService = new LocalIdService();
 
@@ -138,130 +119,4 @@ public class ExampleApplicationSampleE
 	{
 		return params.filePath != "";
 	}
-
-	public static class VolatileRealARGBConverter< T extends RealType< T > > extends AbstractLinearRange implements Converter< Volatile< T >, VolatileARGBType >
-	{
-
-		public VolatileRealARGBConverter( final double min, final double max )
-		{
-			super( min, max );
-		}
-
-		@Override
-		public void convert( final Volatile< T > input, final VolatileARGBType output )
-		{
-			final boolean isValid = input.isValid();
-			output.setValid( isValid );
-			if ( isValid )
-			{
-				final double a = input.get().getRealDouble();
-				final int b = Math.min( 255, roundPositive( Math.max( 0, ( a - min ) / scale * 255.0 ) ) );
-				final int argb = 0xff000000 | ( b << 8 | b ) << 8 | b;
-				output.set( argb );
-			}
-		}
-
-	}
-
-	public static class ARGBConvertedSource< T > implements Source< VolatileARGBType >
-	{
-		final private AbstractViewerSetupImgLoader< T, ? extends Volatile< T > > loader;
-
-		private final int setupId;
-
-		private final Converter< Volatile< T >, VolatileARGBType > converter;
-
-		final protected InterpolatorFactory< VolatileARGBType, RandomAccessible< VolatileARGBType > >[] interpolatorFactories;
-		{
-			interpolatorFactories = new InterpolatorFactory[] {
-					new NearestNeighborInterpolatorFactory< VolatileARGBType >(),
-					new ClampingNLinearInterpolatorFactory< VolatileARGBType >()
-			};
-		}
-
-		public ARGBConvertedSource(
-				final int setupId,
-				final AbstractViewerSetupImgLoader< T, ? extends Volatile< T > > loader,
-				final Converter< Volatile< T >, VolatileARGBType > converter )
-		{
-			this.setupId = setupId;
-			this.loader = loader;
-			this.converter = converter;
-		}
-
-		final public AbstractViewerSetupImgLoader< T, ? extends Volatile< T > > getLoader()
-		{
-			return loader;
-		}
-
-		@Override
-		public RandomAccessibleInterval< VolatileARGBType > getSource( final int t, final int level )
-		{
-			return Converters.convert(
-					loader.getVolatileImage( t, level ),
-					converter,
-					new VolatileARGBType() );
-		}
-
-		@Override
-		public void getSourceTransform( final int t, final int level, final AffineTransform3D transform )
-		{
-			transform.set( loader.getMipmapTransforms()[ level ] );
-		}
-
-		/**
-		 * TODO Store this in a field
-		 */
-		@Override
-		public int getNumMipmapLevels()
-		{
-			return loader.getMipmapResolutions().length;
-		}
-
-		@Override
-		public boolean isPresent( final int t )
-		{
-			return t == 0;
-		}
-
-		@Override
-		public RealRandomAccessible< VolatileARGBType > getInterpolatedSource( final int t, final int level, final Interpolation method )
-		{
-
-			final ExtendedRandomAccessibleInterval< VolatileARGBType, RandomAccessibleInterval< VolatileARGBType > > extendedSource =
-					Views.extendValue( getSource( t, level ), new VolatileARGBType( 0 ) );
-			switch ( method )
-			{
-			case NLINEAR:
-				return Views.interpolate( extendedSource, interpolatorFactories[ 1 ] );
-			default:
-				return Views.interpolate( extendedSource, interpolatorFactories[ 0 ] );
-			}
-		}
-
-		@Override
-		public VolatileARGBType getType()
-		{
-			return new VolatileARGBType();
-		}
-
-		@Override
-		public String getName()
-		{
-			return "1 2 3";
-		}
-
-		@Override
-		public VoxelDimensions getVoxelDimensions()
-		{
-			return null;
-		}
-
-		// TODO: make ARGBType version of this source
-		public Source nonVolatile()
-		{
-			return this;
-		}
-	}
-
 }
