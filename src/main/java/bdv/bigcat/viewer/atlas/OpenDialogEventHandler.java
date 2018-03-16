454e7905f2521658701f17f0e66aee048588e822
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-03-16 13:40:45 -0400

    Use updated LabelMultisetType for unique label lists.

--- src/main/java/bdv/bigcat/viewer/atlas/OpenDialogEventHandler.java
+++ src/main/java/bdv/bigcat/viewer/atlas/OpenDialogEventHandler.java
@@ -1,10 +1,15 @@
 package bdv.bigcat.viewer.atlas;
 
 import java.lang.invoke.MethodHandles;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Optional;
+import java.util.concurrent.ExecutorService;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,19 +19,31 @@ import bdv.bigcat.viewer.atlas.data.LabelDataSource;
 import bdv.bigcat.viewer.atlas.opendialog.BackendDialog;
 import bdv.bigcat.viewer.atlas.opendialog.OpenSourceDialog;
 import bdv.bigcat.viewer.atlas.opendialog.meta.MetaPanel;
+import bdv.bigcat.viewer.meshes.MeshGenerator.ShapeKey;
+import bdv.bigcat.viewer.meshes.cache.CacheUtils;
+import bdv.bigcat.viewer.util.HashWrapper;
 import bdv.util.IdService;
 import bdv.util.LocalIdService;
 import bdv.util.volatiles.SharedQueue;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import net.imglib2.Interval;
+import net.imglib2.IterableInterval;
+import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.cell.AbstractCellImg;
+import net.imglib2.img.cell.Cell;
+import net.imglib2.img.cell.CellGrid;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.label.LabelMultisetType;
+import net.imglib2.type.label.VolatileLabelMultisetArray;
 import net.imglib2.type.label.VolatileLabelMultisetType;
 import net.imglib2.type.numeric.IntegerType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.integer.UnsignedLongType;
 import net.imglib2.type.volatiles.AbstractVolatileRealType;
+import net.imglib2.util.Pair;
 
 public class OpenDialogEventHandler implements EventHandler< Event >
 {
@@ -193,6 +210,9 @@ public class OpenDialogEventHandler implements EventHandler< Event >
 			final Consumer< RandomAccessibleInterval< UnsignedLongType > > mergeCanvasIntoBackground )
 	{
 		LOG.warn( "Adding label multiset source, maybe masked: {} {}", cacheDir, idService );
+		final Function< Long, Interval[] >[] blockListCaches = getBlockListCaches( lsource, viewer.generalPurposeExecutorService() );
+		final int[][] cubeSizes = Stream.generate( () -> new int[] { 1, 1, 1 } ).limit( blockListCaches.length ).toArray( int[][]::new );
+		final Function< ShapeKey, Pair< float[], float[] > >[] meshCaches = blockListCaches == null ? null : CacheUtils.meshCacheLoaders( lsource, cubeSizes, lbl -> ( src, tgt ) -> tgt.set( src.contains( lbl ) ), CacheUtils::toCacheSoftRefLoaderCache );
 		if ( cacheDir != null )
 		{
 			final int[] blockSize = { 64, 64, 64 };
@@ -205,7 +225,63 @@ public class OpenDialogEventHandler implements EventHandler< Event >
 					null );
 		}
 		else
-			viewer.addLabelSource( lsource, lsource.getAssignment(), idService, null, null );
+			viewer.addLabelSource( lsource, lsource.getAssignment(), idService, blockListCaches, meshCaches );
+	}
+
+	public static < C extends Cell< VolatileLabelMultisetArray >, I extends RandomAccessible< C > & IterableInterval< C > > Function< Long, Interval[] >[] getBlockListCaches(
+			final DataSource< LabelMultisetType, ? > source,
+			final ExecutorService es )
+	{
+		final int numLevels = source.getNumMipmapLevels();
+		if ( IntStream.range( 0, numLevels ).mapToObj( lvl -> source.getDataSource( 0, lvl ) ).filter( src -> !( src instanceof AbstractCellImg< ?, ?, ?, ? > ) ).count() > 0 )
+			return null;
+
+		final int[][] blockSizes = IntStream
+				.range( 0, numLevels )
+				.mapToObj( lvl -> ( AbstractCellImg< ?, ?, ?, ? > ) source.getDataSource( 0, lvl ) )
+				.map( AbstractCellImg::getCellGrid )
+				.map( OpenDialogEventHandler::blockSize )
+				.toArray( int[][]::new );
+
+		final double[][] scalingFactors = Atlas.scaleFactorsFromAffineTransforms( source );
+
+		@SuppressWarnings( "unchecked" )
+		final Function< HashWrapper< long[] >, long[] >[] uniqueIdCaches = new Function[ numLevels ];
+
+		for ( int level = 0; level < numLevels; ++level )
+		{
+			@SuppressWarnings( "unchecked" )
+			final AbstractCellImg< LabelMultisetType, VolatileLabelMultisetArray, C, I > img =
+					( AbstractCellImg< LabelMultisetType, VolatileLabelMultisetArray, C, I > ) source.getDataSource( 0, level );
+			uniqueIdCaches[ level ] = uniqueLabelLoaders( img );
+		}
+
+		return CacheUtils.blocksForLabelCaches( source, uniqueIdCaches, blockSizes, scalingFactors, CacheUtils::toCacheSoftRefLoaderCache, es );
+
+	}
+
+	public static int[] blockSize( final CellGrid grid )
+	{
+		final int[] blockSize = new int[ grid.numDimensions() ];
+		Arrays.setAll( blockSize, grid::cellDimension );
+		return blockSize;
+	}
+
+	public static < C extends Cell< VolatileLabelMultisetArray >, I extends RandomAccessible< C > & IterableInterval< C > >
+			Function< HashWrapper< long[] >, long[] > uniqueLabelLoaders(
+					final AbstractCellImg< LabelMultisetType, VolatileLabelMultisetArray, C, I > img )
+	{
+		final I cells = img.getCells();
+		final int[] blockSize = blockSize( img.getCellGrid() );
+		return location -> {
+			final RandomAccess< C > access = cells.randomAccess();
+			final long[] position = location.getData().clone();
+//			Arrays.setAll( position, d -> position[ d ] / blockSize[ d ] );
+			access.setPosition( position );
+			final long[] labels = access.get().getData().containedLabels();
+			LOG.warn( "Position={}: labels={}", position, labels );
+			return labels;
+		};
 	}
 
 }
