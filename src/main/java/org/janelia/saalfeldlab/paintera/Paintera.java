59ed27b42b391706a3a47769349a86bf64a5b097
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2018-04-28 18:37:21 -0700

    move Paintera pseudo-members into start method
    reduce render resolution to 0.5 max if largest screen is >= 2500px

--- src/main/java/org/janelia/saalfeldlab/paintera/Paintera.java
+++ src/main/java/org/janelia/saalfeldlab/paintera/Paintera.java
@@ -12,34 +12,47 @@ import org.slf4j.LoggerFactory;
 import bdv.viewer.ViewerOptions;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import picocli.CommandLine;
 
 public class Paintera extends Application
 {
-
 	private static final Logger LOG = LoggerFactory.getLogger( MethodHandles.lookup().lookupClass() );
-
-	private final PainteraBaseView baseView = new PainteraBaseView(
-			Math.min( 8, Math.max( 1, Runtime.getRuntime().availableProcessors() / 2 ) ),
-			ViewerOptions.options().screenScales( new double[] { 1.0, 0.5, 0.25 } ),
-			si -> s -> si.getState( s ).interpolationProperty().get() );
-
-	private final OrthogonalViews< Viewer3DFX > orthoViews = baseView.orthogonalViews();
-
-	private final KeyTracker keyTracker = new KeyTracker();
-
-	final BorderPaneWithStatusBars paneWithStatus = new BorderPaneWithStatusBars(
-			baseView,
-			keyTracker );
-
-	final PainteraDefaultHandlers defaultHandlers = new PainteraDefaultHandlers( baseView, keyTracker, paneWithStatus );
-
 
 	@Override
 	public void start( final Stage stage ) throws Exception
 	{
+		int maxSize = 0;
+		for ( final Screen screen : Screen.getScreens() )
+		{
+			Rectangle2D bounds = screen.getVisualBounds();
+			maxSize = Math.max( ( int )bounds.getWidth(), maxSize );
+			maxSize = Math.max( ( int )bounds.getHeight(), maxSize );
+		}
+
+		LOG.debug( "max screen size = {}", maxSize );
+
+		final double[] screenScales = maxSize < 2500 ?
+				new double [] { 1.0 / 1.0, 1.0 / 2.0, 1.0 / 4.0, 1.0 / 8.0}:
+				new double [] { 1.0 / 2.0, 1.0 / 4.0, 1.0 / 8.0, 1.0 / 16.0};
+
+		final PainteraBaseView baseView = new PainteraBaseView(
+				Math.min( 8, Math.max( 1, Runtime.getRuntime().availableProcessors() / 2 ) ),
+				ViewerOptions.options().screenScales( screenScales ),
+				si -> s -> si.getState( s ).interpolationProperty().get() );
+
+		final OrthogonalViews< Viewer3DFX > orthoViews = baseView.orthogonalViews();
+
+		final KeyTracker keyTracker = new KeyTracker();
+
+		final BorderPaneWithStatusBars paneWithStatus = new BorderPaneWithStatusBars(
+				baseView,
+				keyTracker );
+
+		final PainteraDefaultHandlers defaultHandlers = new PainteraDefaultHandlers( baseView, keyTracker, paneWithStatus );
 
 		final Parameters parameters = getParameters();
 		final String[] args = parameters.getRaw().stream().toArray( String[]::new );
