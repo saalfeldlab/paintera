8bb0315dd5007bf64fa51328ece94c696bfe4c86
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-05-22 10:39:16 -0400

    Add BlocksForLabelDelegate

---
+++ src/main/java/org/janelia/saalfeldlab/paintera/meshes/cache/BlocksForLabelDelegate.java
@@ -0,0 +1,72 @@
+package org.janelia.saalfeldlab.paintera.meshes.cache;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Function;
+
+import org.janelia.saalfeldlab.paintera.meshes.InterruptibleFunction;
+import org.janelia.saalfeldlab.util.HashWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.imglib2.Interval;
+
+public class BlocksForLabelDelegate< T, U > implements InterruptibleFunction< T, Interval[] >
+{
+
+	private static final Logger LOG = LoggerFactory.getLogger( MethodHandles.lookup().lookupClass() );
+
+	private final InterruptibleFunction< U, Interval[] > delegate;
+
+	private final Function< T, U[] > keyMapping;
+
+	public BlocksForLabelDelegate( final InterruptibleFunction< U, Interval[] > delegate, final Function< T, U[] > keyMapping )
+	{
+		super();
+		this.delegate = delegate;
+		this.keyMapping = keyMapping;
+	}
+
+	@Override
+	public Interval[] apply( final T t )
+	{
+		final Set< HashWrapper< Interval > > intervals = new HashSet<>();
+
+		final U[] mappedKeys = this.keyMapping.apply( t );
+		LOG.debug( "Mapped keys from {} to {}", t, mappedKeys );
+		for ( final U mappedKey : mappedKeys )
+		{
+			LOG.trace( "Requesting intervals from delegate for key {}", mappedKey );
+			final Interval[] intervalsForKey = delegate.apply( mappedKey );
+			LOG.trace( "For key={}, intervals={}", mappedKey, intervalsForKey );
+			Arrays
+			.stream( intervalsForKey )
+			.map( HashWrapper::interval )
+			.forEach( intervals::add );
+		}
+
+		LOG.debug( "Got intervals: {}", intervals );
+
+		return intervals.stream().map( HashWrapper::getData ).toArray( Interval[]::new );
+	}
+
+	@Override
+	public void interruptFor( final T t )
+	{
+		Arrays.stream( keyMapping.apply( t ) ).forEach( delegate::interruptFor );
+	}
+
+	@SuppressWarnings( "unchecked" )
+	public static < T, U > BlocksForLabelDelegate< T, U >[] delegate(
+			final InterruptibleFunction< U, Interval[] >[] delegates,
+			final Function< T, U[] > keyMapping )
+	{
+		return Arrays
+				.stream( delegates )
+				.map( d -> new BlocksForLabelDelegate<>( d, keyMapping ) )
+				.toArray( BlocksForLabelDelegate[]::new );
+	}
+
+}
