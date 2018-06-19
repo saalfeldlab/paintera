16a94919a9e7d0f8f92423befb9377d206a78ede
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-06-19 18:16:10 -0400

    Store settings only for active meshes
    Previously, settings would be stored for all meshes that had been selected at some point during the session. Instead only store settings for those that are currently selected to avoid blowing up attributes.json

--- src/main/java/org/janelia/saalfeldlab/paintera/serialization/sourcestate/LabelSourceStateSerializer.java
+++ src/main/java/org/janelia/saalfeldlab/paintera/serialization/sourcestate/LabelSourceStateSerializer.java
@@ -4,12 +4,16 @@ import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Type;
 
 import org.janelia.saalfeldlab.paintera.control.lock.LockedSegmentsOnlyLocal;
+import org.janelia.saalfeldlab.paintera.control.selection.SelectedSegments;
+import org.janelia.saalfeldlab.paintera.meshes.ManagedMeshSettings;
 import org.janelia.saalfeldlab.paintera.state.LabelSourceState;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
+
+import gnu.trove.set.hash.TLongHashSet;
 
 public class LabelSourceStateSerializer
 		extends SourceStateSerialization.SourceStateSerializerWithoutDependencies< LabelSourceState< ?, ? > >
@@ -30,7 +34,11 @@ public class LabelSourceStateSerializer
 		map.add( SELECTED_IDS_KEY, context.serialize( state.selectedIds(), state.selectedIds().getClass() ) );
 		map.add( ASSIGNMENT_KEY, context.serialize( state.assignment() ) );
 		map.add( LabelSourceStateDeserializer.LOCKED_SEGMENTS_KEY, context.serialize( ( ( LockedSegmentsOnlyLocal ) state.lockedSegments() ).lockedSegmentsCopy() ) );
-		map.add( MANAGED_MESH_SETTINGS_KEY, context.serialize( state.managedMeshSettings() ) );
+		final ManagedMeshSettings managedMeshSettings = new ManagedMeshSettings( state.managedMeshSettings().getGlobalSettings() );
+		managedMeshSettings.set( state.managedMeshSettings() );
+		final TLongHashSet activeSegments = new TLongHashSet( new SelectedSegments( state.selectedIds(), state.assignment() ).getSelectedSegments() );
+		managedMeshSettings.keepOnlyMatching( activeSegments::contains );
+		map.add( MANAGED_MESH_SETTINGS_KEY, context.serialize( managedMeshSettings ) );
 		return map;
 	}
 
