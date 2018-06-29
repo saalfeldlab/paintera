5fba528ae65856b5b6e5fe22bad4b19e7127e927
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-06-29 17:22:45 -0400

    Add install.sh for Linux (and probably OSX)

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+BIN_DIR="${BIN_DIR:-$HOME/local/bin}"
+JRUN_PATH="${BIN_DIR}/jrun"    JRUN_URL="https://raw.githubusercontent.com/ctrueden/jrun/3ddde431f6387d76088a12f1eb3fd8f49ff4091d/jrun"
+
+if [ ! -f "${JRUN_PATH}" ]; then
+    curl "${JRUN_URL}" -o "${JRUN_PATH}"
+fi
+
+chmod u+x "${JRUN_PATH}"
+
+cp paintera "${BIN_DIR}"
