1800e181b0b94d05afa33e15505b45f1d790a388
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-06-29 10:56:15 -0400

    Optimize painting if axis aligned
    TODO:
     - do the same for 2D flood fill
     - respect thickness

--- src/main/java/org/janelia/saalfeldlab/paintera/control/paint/Paint2D.java
+++ src/main/java/org/janelia/saalfeldlab/paintera/control/paint/Paint2D.java
@@ -2,6 +2,7 @@ package org.janelia.saalfeldlab.paintera.control.paint;
 
 import java.lang.invoke.MethodHandles;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,10 +14,15 @@ import net.imglib2.Interval;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessible;
 import net.imglib2.RealPoint;
+import net.imglib2.algorithm.neighborhood.HyperSphereNeighborhood;
+import net.imglib2.algorithm.neighborhood.Neighborhood;
 import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.type.numeric.integer.UnsignedLongType;
 import net.imglib2.util.AccessBoxRandomAccessibleOnGet;
 import net.imglib2.util.Intervals;
+import net.imglib2.view.MixedTransformView;
+import net.imglib2.view.Views;
+import tmp.net.imglib2.algorithm.neighborhood.HyperEllipsoidNeighborhood;
 
 public class Paint2D
 {
@@ -50,30 +56,140 @@ public class Paint2D
 		LOG.debug( "range is {}", zRange );
 
 		final double viewerRadius = Affine3DHelpers.extractScale( globalToViewerTransform, 0 ) * radius;
-		final double radiusX = xRange + viewerRadius;
-		final double radiusY = yRange + viewerRadius;
-		final double[] fillMin = { x - viewerRadius, y - viewerRadius, -zRange };
-		final double[] fillMax = { x + viewerRadius, y + viewerRadius, +zRange };
-		labelToViewerTransform.applyInverse( fillMin, fillMin );
-		labelToViewerTransform.applyInverse( fillMax, fillMax );
-		final double[] transformedFillMin = new double[ 3 ];
-		final double[] transformedFillMax = new double[ 3 ];
-		Arrays.setAll( transformedFillMin, d -> ( long ) Math.floor( Math.min( fillMin[ d ], fillMax[ d ] ) ) );
-		Arrays.setAll( transformedFillMax, d -> ( long ) Math.ceil( Math.max( fillMin[ d ], fillMax[ d ] ) ) );
+		final int viewerAxisInLabelCoordinates = PaintUtils.labelAxisCorrespondingToViewerAxis(
+				labelToGlobalTransform,
+				globalToViewerTransform,
+				2 );
+		LOG.debug( "Got coresspanding viewer axis in label coordinate system: {}", viewerAxisInLabelCoordinates );
 
-		// containingInterval might be too small
-		final Interval conatiningInterval = Intervals.smallestContainingInterval( new FinalRealInterval( transformedFillMin, transformedFillMax ) );
-		final AccessBoxRandomAccessibleOnGet< UnsignedLongType > accessTracker = labels instanceof AccessBoxRandomAccessibleOnGet< ? >
-				? ( AccessBoxRandomAccessibleOnGet< UnsignedLongType > ) labels
-				: new AccessBoxRandomAccessibleOnGet<>( labels );
-		accessTracker.initAccessBox();
-		final RandomAccess< UnsignedLongType > access = accessTracker.randomAccess( conatiningInterval );
-		final RealPoint seed = new RealPoint( x, y, 0.0 );
+		if ( viewerAxisInLabelCoordinates < 0 )
+		{
+			final double radiusX = xRange + viewerRadius;
+			final double radiusY = yRange + viewerRadius;
+			final double[] fillMin = { x - viewerRadius, y - viewerRadius, -zRange };
+			final double[] fillMax = { x + viewerRadius, y + viewerRadius, +zRange };
+			labelToViewerTransform.applyInverse( fillMin, fillMin );
+			labelToViewerTransform.applyInverse( fillMax, fillMax );
+			final double[] transformedFillMin = new double[ 3 ];
+			final double[] transformedFillMax = new double[ 3 ];
+			Arrays.setAll( transformedFillMin, d -> ( long ) Math.floor( Math.min( fillMin[ d ], fillMax[ d ] ) ) );
+			Arrays.setAll( transformedFillMax, d -> ( long ) Math.ceil( Math.max( fillMin[ d ], fillMax[ d ] ) ) );
 
-		FloodFillTransformedCylinder3D.fill( labelToViewerTransform, radiusX, radiusY, zRange, access, seed, fillLabel );
+			// containingInterval might be too small
+			final Interval conatiningInterval = Intervals.smallestContainingInterval( new FinalRealInterval( transformedFillMin, transformedFillMax ) );
+			final AccessBoxRandomAccessibleOnGet< UnsignedLongType > accessTracker = labels instanceof AccessBoxRandomAccessibleOnGet< ? >
+					? ( AccessBoxRandomAccessibleOnGet< UnsignedLongType > ) labels
+					: new AccessBoxRandomAccessibleOnGet<>( labels );
+			accessTracker.initAccessBox();
+			final RandomAccess< UnsignedLongType > access = accessTracker.randomAccess( conatiningInterval );
+			final RealPoint seed = new RealPoint( x, y, 0.0 );
 
-		final FinalInterval trackedInterval = new FinalInterval( accessTracker.getMin(), accessTracker.getMax() );
-		return trackedInterval;
+			FloodFillTransformedCylinder3D.fill( labelToViewerTransform, radiusX, radiusY, zRange, access, seed, fillLabel );
+
+			final FinalInterval trackedInterval = new FinalInterval( accessTracker.getMin(), accessTracker.getMax() );
+			return trackedInterval;
+		}
+		else
+		{
+			LOG.debug( "Painting axis aligned (optimized)" );
+			final double[] transformedRadius = new double[ 3 ];
+
+			final int correspondingToXAxis = PaintUtils.labelAxisCorrespondingToViewerAxis(
+					labelToGlobalTransform,
+					globalToViewerTransform,
+					0 );
+			final int correspondingToYAxis = PaintUtils.labelAxisCorrespondingToViewerAxis(
+					labelToGlobalTransform,
+					globalToViewerTransform,
+					1 );
+
+			LOG.warn(
+					"Corresponding axes in label coordinate system: x={} y={} z={}",
+					correspondingToXAxis,
+					correspondingToYAxis,
+					viewerAxisInLabelCoordinates );
+
+			final double[] transformedXRadius = PaintUtils.viewerAxisInLabelCoordinates(
+					labelToGlobalTransform,
+					globalToViewerTransform,
+					0,
+					viewerRadius );
+			final double[] transformedYRadius = PaintUtils.viewerAxisInLabelCoordinates(
+					labelToGlobalTransform,
+					globalToViewerTransform,
+					1,
+					viewerRadius );
+			LOG.warn( "Transformed radii: x={} y={}", transformedXRadius, transformedYRadius );
+
+			transformedRadius[ correspondingToXAxis ] = transformedXRadius[ correspondingToXAxis ];
+			transformedRadius[ correspondingToYAxis ] = transformedYRadius[ correspondingToYAxis ];
+
+			final double[] seed = new double[] { x, y, 0.0 };
+			labelToViewerTransform.applyInverse( seed, seed );
+
+			final long slicePos = Math.round( seed[ viewerAxisInLabelCoordinates ] );
+			final MixedTransformView< UnsignedLongType > slice = Views.hyperSlice(
+					labels,
+					viewerAxisInLabelCoordinates,
+					slicePos );
+
+			final long[] center = {
+					Math.round( seed[ viewerAxisInLabelCoordinates == 0 ? 1 : 0 ] ),
+					Math.round( seed[ viewerAxisInLabelCoordinates != 2 ? 2 : 1 ] )
+			};
+
+			final long[] sliceRadii = {
+					( long ) Math.ceil( transformedRadius[ viewerAxisInLabelCoordinates == 0 ? 1 : 0 ] ),
+					( long ) Math.ceil( transformedRadius[ viewerAxisInLabelCoordinates != 2 ? 2 : 1 ] )
+			};
+
+			LOG.warn( "Transformed radius={}", transformedRadius );
+
+			final Neighborhood< UnsignedLongType > neighborhood;
+			if ( sliceRadii[ 0 ] == sliceRadii[ 1 ] )
+			{
+				neighborhood = HyperSphereNeighborhood
+						.< UnsignedLongType >factory()
+						.create(
+								center,
+								sliceRadii[ 0 ],
+								slice.randomAccess() );
+			}
+			else
+			{
+				neighborhood = HyperEllipsoidNeighborhood
+						.< UnsignedLongType >factory()
+						.create(
+								center,
+								sliceRadii,
+								slice.randomAccess() );
+			}
+
+			LOG.warn( "Painting with radii {} centered at {} in slice {}", sliceRadii, center, slicePos );
+
+			for ( final UnsignedLongType t : neighborhood )
+			{
+				t.set( fillLabel );
+			}
+
+			final long[] min2D = IntStream.range( 0, 2 ).mapToLong( d -> center[ d ] - sliceRadii[ d ] ).toArray();
+			final long[] max2D = IntStream.range( 0, 2 ).mapToLong( d -> center[ d ] + sliceRadii[ d ] ).toArray();
+
+			final long[] min = {
+					viewerAxisInLabelCoordinates == 0 ? slicePos : min2D[ 0 ],
+					viewerAxisInLabelCoordinates == 1 ? slicePos : viewerAxisInLabelCoordinates == 0 ? min2D[ 0 ] : min2D[ 1 ],
+					viewerAxisInLabelCoordinates == 2 ? slicePos : min2D[ 1 ]
+			};
+
+			final long[] max = {
+					viewerAxisInLabelCoordinates == 0 ? slicePos : max2D[ 0 ],
+					viewerAxisInLabelCoordinates == 1 ? slicePos : viewerAxisInLabelCoordinates == 0 ? max2D[ 0 ] : max2D[ 1 ],
+					viewerAxisInLabelCoordinates == 2 ? slicePos : max2D[ 1 ]
+			};
+
+			return new FinalInterval( min, max );
+
+		}
 	}
 
 }
--- src/main/java/org/janelia/saalfeldlab/paintera/control/paint/PaintUtils.java
+++ src/main/java/org/janelia/saalfeldlab/paintera/control/paint/PaintUtils.java
@@ -13,6 +13,74 @@ public class PaintUtils
 
 	private static final Logger LOG = LoggerFactory.getLogger( MethodHandles.lookup().lookupClass() );
 
+	public static int labelAxisCorrespondingToViewerAxis(
+			final AffineTransform3D labelToGlobalTransform,
+			final AffineTransform3D viewerTransform,
+			final int axis )
+	{
+		return labelAxisCorrespondingToViewerAxis( labelToGlobalTransform, viewerTransform, axis, 0.0 );
+	}
+
+	/**
+	 *
+	 * @param labelToGlobalTransform
+	 * @param viewerTransform
+	 * @param axis
+	 *            0=x, 1=y, 2=z
+	 * @param tolerance
+	 *            0 <= tolerance << 1
+	 * @return -1 if axis no unique corresponding axis according to tolerance,
+	 *         else 0=x, 1=y, 2=z
+	 */
+	public static int labelAxisCorrespondingToViewerAxis(
+			final AffineTransform3D labelToGlobalTransform,
+			final AffineTransform3D viewerTransform,
+			final int axis,
+			final double tolerance )
+	{
+		final double[] transformedAxis = viewerAxisInLabelCoordinates( labelToGlobalTransform, viewerTransform, axis );
+		LinAlgHelpers.normalize( transformedAxis );
+		if ( Math.abs( Math.abs( transformedAxis[ 0 ] ) - 1.0 ) <= tolerance ) { return 0; }
+		if ( Math.abs( Math.abs( transformedAxis[ 1 ] ) - 1.0 ) <= tolerance ) { return 1; }
+		if ( Math.abs( Math.abs( transformedAxis[ 2 ] ) - 1.0 ) <= tolerance ) { return 2; }
+		return -1;
+	}
+
+	public static double[] viewerAxisInLabelCoordinates(
+			final AffineTransform3D labelToGlobalTransform,
+			final AffineTransform3D viewerTransform,
+			final int axis )
+	{
+		return viewerAxisInLabelCoordinates( labelToGlobalTransform, viewerTransform, axis, 1.0 );
+	}
+
+	/**
+	 *
+	 * @param labelToGlobalTransform
+	 * @param viewerTransform
+	 * @param axis
+	 *            0=x, 1=y, 2=z
+	 * @param length
+	 *            != 0
+	 * @return
+	 */
+	public static double[] viewerAxisInLabelCoordinates(
+			final AffineTransform3D labelToGlobalTransform,
+			final AffineTransform3D viewerTransform,
+			final int axis,
+			final double length )
+	{
+		final AffineTransform3D labelToGlobalTransformWithoutTranslation = duplicateWithoutTranslation( labelToGlobalTransform );
+		final AffineTransform3D viewerTransformWithoutTranslation = duplicateWithoutTranslation( viewerTransform );
+		final AffineTransform3D labelToViewerTransformWithoutTranslation = labelToGlobalTransformWithoutTranslation.preConcatenate( viewerTransformWithoutTranslation );
+
+		final double[] viewerUnitAxis = { 0.0, 0.0, 0.0 };
+		viewerUnitAxis[ axis ] = length;
+
+		labelToViewerTransformWithoutTranslation.applyInverse( viewerUnitAxis, viewerUnitAxis );
+		return viewerUnitAxis;
+	}
+
 	/**
 	 *
 	 * This should be equivalent to
@@ -26,12 +94,13 @@ public class PaintUtils
 			final AffineTransform3D labelToGlobalTransform,
 			final AffineTransform3D viewerTransform )
 	{
-		final double[] unitX = { 1.0, 0.0, 0.0 };
-		final double[] unitY = { 0.0, 1.0, 0.0 };
-		final double[] unitZ = { 0.0, 0.0, 1.0 };
 		final AffineTransform3D labelToGlobalTransformWithoutTranslation = duplicateWithoutTranslation( labelToGlobalTransform );
 		final AffineTransform3D viewerTransformWithoutTranslation = duplicateWithoutTranslation( viewerTransform );
 		final AffineTransform3D labelToViewerTransformWithoutTranslation = labelToGlobalTransformWithoutTranslation.preConcatenate( viewerTransformWithoutTranslation );
+
+		final double[] unitX = { 1.0, 0.0, 0.0 };
+		final double[] unitY = { 0.0, 1.0, 0.0 };
+		final double[] unitZ = { 0.0, 0.0, 1.0 };
 		labelToViewerTransformWithoutTranslation.applyInverse( unitX, unitX );
 		labelToViewerTransformWithoutTranslation.applyInverse( unitY, unitY );
 		labelToViewerTransformWithoutTranslation.applyInverse( unitZ, unitZ );
@@ -69,9 +138,9 @@ public class PaintUtils
 		viewerTransformWithoutTranslation.apply( unitZ, unitZ );
 		LOG.debug( "Transformed unit vectors x={} y={} z={}", unitX, unitY, unitZ );
 		final double[] projections = new double[] {
-				( Math.abs( unitX[ 0 ] ) + Math.abs( unitY[ 0 ] ) + Math.abs( unitZ[ 0 ] ) ),
-				( Math.abs( unitX[ 1 ] ) + Math.abs( unitY[ 1 ] ) + Math.abs( unitZ[ 1 ] ) ),
-				( Math.abs( unitX[ 2 ] ) + Math.abs( unitY[ 2 ] ) + Math.abs( unitZ[ 2 ] ) )
+				Math.abs( unitX[ 0 ] ) + Math.abs( unitY[ 0 ] ) + Math.abs( unitZ[ 0 ] ),
+				Math.abs( unitX[ 1 ] ) + Math.abs( unitY[ 1 ] ) + Math.abs( unitZ[ 1 ] ),
+				Math.abs( unitX[ 2 ] ) + Math.abs( unitY[ 2 ] ) + Math.abs( unitZ[ 2 ] )
 		};
 		LOG.debug( "Projections={}", projections );
 		return projections;
