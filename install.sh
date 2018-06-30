2f9eb3b247954388bf9571a14700fbf3617914ae
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-06-30 11:34:47 -0400

    use prefix rather than BIN_DIR

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-BIN_DIR="${BIN_DIR:-$HOME/local/bin}"
+PREFIX="${PREFIX:-$HOME/local}"
+BIN_DIR="${PREFIX}/bin"
 JRUN_PATH="${BIN_DIR}/jrun"    JRUN_URL="https://raw.githubusercontent.com/ctrueden/jrun/3ddde431f6387d76088a12f1eb3fd8f49ff4091d/jrun"
 
 if [ ! -f "${JRUN_PATH}" ]; then
