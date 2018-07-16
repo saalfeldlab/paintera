e5ee3889f365bb832871b574948c40028926fe02
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-07-16 11:22:59 -0400

    Add JFXUtil

---
+++ src/main/java/org/janelia/saalfeldlab/fx/util/JFXUtil.java
@@ -0,0 +1,18 @@
+package org.janelia.saalfeldlab.fx.util;
+
+import com.sun.javafx.application.PlatformImpl;
+
+public class JFXUtil
+{
+
+	public static void platformImplStartup()
+	{
+		platformImplStartup( () -> {} );
+	}
+
+	public static void platformImplStartup( final Runnable r )
+	{
+		PlatformImpl.startup( r );
+	}
+
+}
