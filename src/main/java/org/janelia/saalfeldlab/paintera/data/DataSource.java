1466c830e811caae522a7d0a92e2e072844631db
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-10-05 16:50:28 -0400

    Add documentation and tests for convenience methods of DataSource

--- src/main/java/org/janelia/saalfeldlab/paintera/data/DataSource.java
+++ src/main/java/org/janelia/saalfeldlab/paintera/data/DataSource.java
@@ -1,46 +1,55 @@
 package org.janelia.saalfeldlab.paintera.data;
 
-import java.util.Arrays;
-
 import bdv.viewer.Interpolation;
 import bdv.viewer.Source;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.RealRandomAccessible;
 import net.imglib2.realtransform.AffineTransform3D;
-import org.janelia.saalfeldlab.paintera.cache.Invalidate;
 import org.janelia.saalfeldlab.paintera.cache.InvalidateAll;
 
+import java.util.Arrays;
+
 /**
- * {@link Source} that includes a type <code>D</code> representation that is used for data processing (in contrast to
- * <code>T</code> that is used for visualization).
+ * {@link Source} that includes a type {@code D} representation that is used for data processing (in contrast to
+ * {@code T}that is used for visualization).
  */
 public interface DataSource<D, T> extends Source<T>, InvalidateAll
 {
-	public RandomAccessibleInterval<D> getDataSource(int t, int level);
+	RandomAccessibleInterval<D> getDataSource(int t, int level);
 
-	public RealRandomAccessible<D> getInterpolatedDataSource(final int t, final int level, final Interpolation method);
+	RealRandomAccessible<D> getInterpolatedDataSource(final int t, final int level, final Interpolation method);
 
-	public D getDataType();
+	D getDataType();
 
-	default public int tMin()
-	{
-		return 0;
-	}
-
-	default public int tMax()
-	{
-		return 0;
-	}
-
-	public static double[] getScale(final Source<?> source, final int t, final int level)
+	/**
+	 * Convenience method to extract the scale of a {@link Source} from the diagonal of the {@link AffineTransform3D} at
+	 * {@code t} and {@code level}.
+	 * @param source Extract scale from this source
+	 * @param t Extract scale at this time points
+	 * @param level Extract scale at this mipmap level
+	 * @return diagonal of transform of {@code source} at time {@code t} and level {@code level}.
+	 */
+	static double[] getScale(final Source<?> source, final int t, final int level)
 	{
 		final AffineTransform3D transform = new AffineTransform3D();
 		source.getSourceTransform(t, level, transform);
 		return new double[] {transform.get(0, 0), transform.get(1, 1), transform.get(2, 2)};
 	}
 
-	public static double[] getRelativeScales(final Source<?> source, final int t, final int level, final int
-			targetLevel)
+	/**
+	 * Convenience method to extract the relative scale of two levels of a {@link Source} at time {@code t}.
+	 * @param source Extract relative scale from this source
+	 * @param t Extract relative scale at this time points
+	 * @param level source level
+	 * @param targetLevel target level
+	 * @return ratio of diagonals of transforms at levels {@code targetLevel} and {@code level} for {@code source} at time {@code t}:
+	 * scale[targetLevel] / scale[level]
+	 */
+	static double[] getRelativeScales(
+			final Source<?> source,
+			final int t,
+			final int level,
+			final int targetLevel)
 	{
 		final double[] scale       = getScale(source, t, level);
 		final double[] targetScale = getScale(source, t, targetLevel);
---
+++ src/test/java/org/janelia/saalfeldlab/paintera/data/DataSourceTest.java
@@ -0,0 +1,127 @@
+package org.janelia.saalfeldlab.paintera.data;
+
+import bdv.viewer.Interpolation;
+import mpicbg.spim.data.sequence.VoxelDimensions;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.RealRandomAccessible;
+import net.imglib2.realtransform.AffineTransform3D;
+import net.imglib2.type.numeric.integer.ByteType;
+import org.janelia.saalfeldlab.util.n5.N5Helpers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Arrays;
+
+public class DataSourceTest {
+
+	private static Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	private static class DummyDataSource implements DataSource<ByteType, ByteType>
+	{
+
+		private final AffineTransform3D[] transforms;
+
+		private DummyDataSource(AffineTransform3D[] transforms) {
+			this.transforms = transforms;
+		}
+
+		@Override
+		public RandomAccessibleInterval<ByteType> getDataSource(int t, int level) {
+			return null;
+		}
+
+		@Override
+		public RealRandomAccessible<ByteType> getInterpolatedDataSource(int t, int level, Interpolation method) {
+			return null;
+		}
+
+		@Override
+		public ByteType getDataType() {
+			return null;
+		}
+
+		@Override
+		public boolean isPresent(int i) {
+			return false;
+		}
+
+		@Override
+		public RandomAccessibleInterval<ByteType> getSource(int i, int i1) {
+			return null;
+		}
+
+		@Override
+		public RealRandomAccessible<ByteType> getInterpolatedSource(int i, int i1, Interpolation interpolation) {
+			return null;
+		}
+
+		@Override
+		public void getSourceTransform(int t, int level, AffineTransform3D tf) {
+			tf.set(transforms[level]);
+		}
+
+		@Override
+		public ByteType getType() {
+			return null;
+		}
+
+		@Override
+		public String getName() {
+			return null;
+		}
+
+		@Override
+		public VoxelDimensions getVoxelDimensions() {
+			return null;
+		}
+
+		@Override
+		public int getNumMipmapLevels() {
+			return transforms.length;
+		}
+
+		@Override
+		public void invalidateAll() {
+
+		}
+	}
+
+	@Test
+	public void testGetScale()
+	{
+		double[][] resolutions = {
+				{1.0, 1.0, 1.0},
+				{2.0, 3.0, 4.0},
+				{6.0, 12.0, 20.0}
+		};
+
+		double[] zeroOffset = new double[] {0.0, 0.0, 0.0};
+		double[] ratio = new double[3];
+
+				AffineTransform3D[] transforms = Arrays
+				.stream(resolutions)
+				.map(r -> N5Helpers.fromResolutionAndOffset(r, zeroOffset))
+				.toArray(AffineTransform3D[]::new);
+
+		DummyDataSource source = new DummyDataSource(transforms);
+		Assert.assertEquals(resolutions.length, source.getNumMipmapLevels());
+		for (int level = 0; level < source.getNumMipmapLevels(); ++level)
+		{
+			Assert.assertArrayEquals(resolutions[level], DataSource.getScale(source, 0, level), 0.0);
+			for (int targetLevel = 0; targetLevel < source.getNumMipmapLevels(); ++targetLevel)
+			{
+				ratio[0] = resolutions[targetLevel][0] / resolutions[level][0];
+				ratio[1] = resolutions[targetLevel][1] / resolutions[level][1];
+				ratio[2] = resolutions[targetLevel][2] / resolutions[level][2];
+				LOG.debug("Ratio for {} {} is {}", level, targetLevel, ratio);
+				Assert.assertArrayEquals(ratio, DataSource.getRelativeScales(source, 0, level, targetLevel), 0.0);
+			}
+		}
+
+		
+	}
+
+}
