e1f235034df07f089cd7b7e792ccc6cbdaa2372c
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-11-11 14:17:29 -0500

    Add convenience method for resizable dialogs

--- src/main/java/org/janelia/saalfeldlab/paintera/ui/PainteraAlerts.java
+++ src/main/java/org/janelia/saalfeldlab/paintera/ui/PainteraAlerts.java
@@ -7,12 +7,25 @@ public class PainteraAlerts {
 
 	/**
 	 *
+	 * delegates to {@link #alert(Alert.AlertType, boolean)} with {@code isResizable = true}.
+	 *
 	 * @param type type of alert
 	 * @return {@link Alert} with the title set to {@link Paintera#NAME}
 	 */
 	public static Alert alert(final Alert.AlertType type) {
+		return alert(type, true);
+	}
+
+	/**
+	 *
+	 * @param type type of alert
+	 * @param isResizable set to {@code true} if dialog should be resizable
+	 * @return {@link Alert} with the title set to {@link Paintera#NAME}
+	 */
+	public static Alert alert(final Alert.AlertType type, boolean isResizable) {
 		final Alert alert = new Alert(type);
 		alert.setTitle(Paintera.NAME);
+		alert.setResizable(isResizable);
 		return alert;
 	}
 }
