aa6bd0f76c63484fc3b540dbc0fc90a047629083
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-07-14 16:13:44 -0400

    Expose font size to UI

--- src/main/java/org/janelia/saalfeldlab/paintera/config/ScaleBarOverlayConfigNode.java
+++ src/main/java/org/janelia/saalfeldlab/paintera/config/ScaleBarOverlayConfigNode.java
@@ -2,18 +2,15 @@ package org.janelia.saalfeldlab.paintera.config;
 
 import bdv.fx.viewer.scalebar.ScaleBarOverlayConfig;
 import javafx.beans.property.DoubleProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.Node;
-import javafx.scene.Parent;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
-import org.janelia.saalfeldlab.fx.TitledPanes;
+import javafx.scene.text.Font;
 import org.janelia.saalfeldlab.fx.ui.NumberField;
 import org.janelia.saalfeldlab.fx.ui.ObjectField;
 
@@ -27,6 +24,10 @@ public class ScaleBarOverlayConfigNode extends TitledPane {
 
 	private final ColorPicker backgroundColorPicker = new ColorPicker();
 
+	private final ObjectProperty<Font> font = new SimpleObjectProperty<>(new Font("SansSerif", 18.0));
+
+	private final NumberField<DoubleProperty> fontSize = NumberField.doubleField(font.get().getSize(), v -> v > 0.0, ObjectField.SubmitOn.values());
+
 	public ScaleBarOverlayConfigNode()
 	{
 		super("Scale Bar", null);
@@ -36,18 +37,24 @@ public class ScaleBarOverlayConfigNode extends TitledPane {
 		setExpanded(false);
 		grid.add(new Label("Scale Bar Size"), 0, 0);
 		grid.add(targetScaleBarLength.textField(), 1, 0);
-		grid.add(new Label("Foreground Color"), 0, 1);
-		grid.add(new Label("Background Color"), 0, 2);
-		grid.add(foregroundColorPicker, 1, 1);
-		grid.add(backgroundColorPicker, 1, 2);
+		grid.add(new Label("Font Size"), 0, 1);
+		grid.add(fontSize.textField(), 1, 1);
+		grid.add(new Label("Foreground Color"), 0, 2);
+		grid.add(new Label("Background Color"), 0, 3);
+		grid.add(foregroundColorPicker, 1, 2);
+		grid.add(backgroundColorPicker, 1, 3);
 		grid.setHgap(5);
 
 		GridPane.setHgrow(targetScaleBarLength.textField(), Priority.ALWAYS);
+		GridPane.setHgrow(fontSize.textField(), Priority.ALWAYS);
 		GridPane.setHgrow(foregroundColorPicker, Priority.ALWAYS);
 		GridPane.setHgrow(backgroundColorPicker, Priority.ALWAYS);
 
 		foregroundColorPicker.setMaxWidth(Double.POSITIVE_INFINITY);
 		backgroundColorPicker.setMaxWidth(Double.POSITIVE_INFINITY);
+
+		font.addListener((obs, oldv, newv) -> fontSize.valueProperty().set(newv.getSize()));
+		fontSize.valueProperty().addListener((obs, oldv, newv) -> font.set(new Font(font.get().getName(), newv.doubleValue())));
 	}
 
 	public void bindBidirectionalTo(final ScaleBarOverlayConfig config) {
@@ -55,9 +62,13 @@ public class ScaleBarOverlayConfigNode extends TitledPane {
 		this.isShowing.selectedProperty().bindBidirectional(config.isShowingProperty());
 		this.foregroundColorPicker.valueProperty().bindBidirectional(config.foregroundColorProperty());
 		this.backgroundColorPicker.valueProperty().bindBidirectional(config.backgroundColorProperty());
+		this.font.bindBidirectional(config.overlayFontProperty());
 
 		this.targetScaleBarLength.valueProperty().set(config.getTargetScaleBarLength());
 		this.isShowing.setSelected(config.getIsShowing());
+		this.foregroundColorPicker.setValue(config.getForegroundColor());
+		this.backgroundColorPicker.setValue(config.getBackgroundColor());
+		this.font.set(config.getOverlayFont());
 	}
 
 }
