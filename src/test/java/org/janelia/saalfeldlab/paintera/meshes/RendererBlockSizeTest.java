a65af21b0fca9ae92e6f76b8576aec6738d002fd
Igor Pisarev <pisarevi@janelia.hhmi.org>
2019-07-19 20:44:26 -0400

    add more tests for renderer block size calculation

--- src/test/java/org/janelia/saalfeldlab/paintera/meshes/RendererBlockSizeTest.java
+++ src/test/java/org/janelia/saalfeldlab/paintera/meshes/RendererBlockSizeTest.java
@@ -29,4 +29,33 @@ public class RendererBlockSizeTest
 		Assert.assertArrayEquals(new int[] {64, 64, 64}, blockSizes[4]);
 		Assert.assertArrayEquals(new int[] {64, 64, 64}, blockSizes[5]);
 	}
+
+	@Test
+	public void testLargeValues()
+	{
+		final int[][] blockSizes = MeshGeneratorJobManager.getRendererFullBlockSizes(1024, new double[][] {
+				{4, 4, 40}, {8, 8, 40}, {16, 16, 40}, {32, 32, 40}, {64, 64, 80}, {128, 128, 80}
+			});
+		Assert.assertArrayEquals(new int[] {1024, 1024, 102}, blockSizes[0]);
+		Assert.assertArrayEquals(new int[] {1024, 1024, 204}, blockSizes[1]);
+		Assert.assertArrayEquals(new int[] {1024, 1024, 408}, blockSizes[2]);
+		Assert.assertArrayEquals(new int[] {1024, 1024, 816}, blockSizes[3]);
+		Assert.assertArrayEquals(new int[] {1024, 1024, 816}, blockSizes[4]);
+		Assert.assertArrayEquals(new int[] {1024, 1024, 816}, blockSizes[5]);
+	}
+
+	@Test
+	public void testLargeValuesExtremeResolution()
+	{
+		final int[][] blockSizes = MeshGeneratorJobManager.getRendererFullBlockSizes(1024, new double[][] {
+				{4, 4, 160}, {8, 8, 160}, {16, 16, 160}, {32, 32, 160}, {64, 64, 160}, {128, 128, 160}, {256, 256, 160}
+			});
+		Assert.assertArrayEquals(new int[] {1024, 1024, 26}, blockSizes[0]);
+		Assert.assertArrayEquals(new int[] {1024, 1024, 52}, blockSizes[1]);
+		Assert.assertArrayEquals(new int[] {1024, 1024, 104}, blockSizes[2]);
+		Assert.assertArrayEquals(new int[] {1024, 1024, 208}, blockSizes[3]);
+		Assert.assertArrayEquals(new int[] {1024, 1024, 416}, blockSizes[4]);
+		Assert.assertArrayEquals(new int[] {1024, 1024, 832}, blockSizes[5]);
+		Assert.assertArrayEquals(new int[] {1024, 1024, 832}, blockSizes[6]);
+	}
 }
