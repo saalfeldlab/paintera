480110212408d054b2afe97ca26a5043bdab01e0
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-08-05 17:25:04 -0400

    Update bash completion script

--- generate-bash-completion.kts
+++ generate-bash-completion.kts
@@ -1,7 +1,7 @@
 #!/usr/bin/env kscript
 
 @file:KotlinOpts("-classpath /usr/lib/jvm/java-8-openjdk/jre/lib/ext/jfxrt.jar")
-@file:DependsOn("org.janelia.saalfeldlab:paintera:0.17.0")
+@file:DependsOn("org.janelia.saalfeldlab:paintera:0.17.1-SNAPSHOT")
 @file:DependsOn("org.slf4j:slf4j-simple:1.7.25")
 
 import picocli.AutoComplete
