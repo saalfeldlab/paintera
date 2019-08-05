0c3723dff7b4c218b9ed6d2b9b5ecb81d9ca7274
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-08-05 14:38:20 -0400

    Add generate-bash-completion script

---
+++ generate-bash-completion.kts
@@ -0,0 +1,14 @@
+#!/usr/bin/env kscript
+
+@file:KotlinOpts("-classpath /usr/lib/jvm/java-8-openjdk/jre/lib/ext/jfxrt.jar")
+@file:DependsOn("org.janelia.saalfeldlab:paintera:0.17.0")
+@file:DependsOn("org.slf4j:slf4j-simple:1.7.25")
+
+import picocli.AutoComplete
+
+AutoComplete.main(
+        "-n",
+        "paintera",
+        "org.janelia.saalfeldlab.paintera.PainteraCommandLineArgs",
+        "--force")
+
