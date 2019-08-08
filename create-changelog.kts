cce44083bf87da4b518ce17aad3c6fc3efe69036
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-08-08 08:03:22 -0400

    Update changelog creation script

--- create-changelog.kts
+++ create-changelog.kts
@@ -1,10 +1,18 @@
 #!/usr/bin/env kscript
 
 @file:DependsOn("org.json:json:20180813")
+@file:DependsOn("org.apache.maven:maven-model:3.6.1")
+@file:DependsOn("org.eclipse.jgit:org.eclipse.jgit:5.4.0.201906121030-r")
+@file:DependsOn("org.slf4j:slf4j-simple:1.7.25")
 
+import org.apache.maven.model.Model
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -15,9 +23,64 @@ data class Change(val author: String, val message: String, val date: String) {
 			date = (((commit["commit"] as JSONObject)["author"] as JSONObject)).getString("date"))
 }
 
+fun versionFromString(str: String): Version {
+        return if (str.contains("-")) {
+                val split = str.split("-")
+                val Mmp   = split[0].split(".").map { it.toInt() }.toIntArray()
+                Version(Mmp[0], Mmp[1], Mmp[2], split[1])
+        } else {
+                val Mmp = str.split(".").map { it.toInt() }.toIntArray()
+                Version(Mmp[0], Mmp[1], Mmp[2], null)
+        }
+}
+
+data class Version(
+        val major: Int,
+        val minor: Int,
+        val patch: Int,
+        val preRelease: String? = null): Comparable<Version> {
+
+        val versionString = preRelease?.let { "$major.$minor.$patch-$preRelease" } ?: "$major.$minor.$patch"
+
+        public constructor(params: Array<String>): this(
+                params[0].toInt(),
+                params[1].toInt(),
+                params[2].toInt(),
+                if (params.size == 4) params[3] else null)
+
+        override fun toString() = versionString
+
+        override fun compareTo(other: Version): Int {
+                val majorComp = major.compareTo(other.major)
+                if (majorComp != 0)
+                        return majorComp
+                val minorComp = minor.compareTo(other.minor)
+                if (minorComp != 0)
+                        return minorComp
+                val patchComp = patch.compareTo(other.patch)
+                if (patchComp != 0)
+                        return patchComp
+                if (preRelease === null && other.preRelease != null)
+                        return 1
+                if (other.preRelease === null && preRelease != null)
+                        return -1
+                else if (other.preRelease === null && preRelease === null)
+                        return 0
+                return preRelease!!.compareTo(other.preRelease!!)
+        }
+}
+
 val GITHUB_API_URL = "https://api.github.com";
 
 fun getTags(repo: String) = JSONArray(fromURL(URL("$GITHUB_API_URL/repos/$repo/tags"))).filterIsInstance<JSONObject>()
+
+fun getCommitDate(
+        repo: String,
+        commit: String): String {
+	val commitUrl = URL("$GITHUB_API_URL/repos/$repo/commits/$commit")
+	val commitDate = JSONObject(fromURL(commitUrl)).getJSONObject("commit").getJSONObject("author").getString("date")
+	return commitDate
+}
 
 fun getPreviousTag(tagName: String, tags: List<JSONObject>) = tags[tags.indexOfFirst { it.getString("name") == tagName } + 1]
 
@@ -117,28 +180,67 @@ fun fromURL(url: URL): String {
 	return text
 }
 
-val version = if (args.size > 0) args[0] else "0.18.0"
-val commits = getCommitsBetweenReleases("saalfeldlab", "paintera", version, true)
-val mergeCommits = commits.filter { it.message.startsWith("Merge pull request #") }
-mergeCommits.forEach { println(it) }
+val reader = MavenXpp3Reader()
+val model  = FileReader("pom.xml").use { reader.read(it) }
+val isSnapshot = model.version.contains("-SNAPSHOT")
+val version = versionFromString(model.version)
+
+val tags = getCommitTags("saalfeldlab/paintera").filter {
+        try {
+                versionFromString(it.getString("name").replace("paintera-", ""))
+                true
+        } catch (e: Exception) {
+                false
+        }
+}
+
+val repo = FileRepositoryBuilder().setGitDir(File(".git")).build()
+val head = repo.resolve("HEAD")
+val commitTo = if (isSnapshot) {
+        head.name
+} else {
+        "paintera-${model.version}"
+                        .takeUnless { t -> tags.count { it.getString("name") == t } == 0 }
+                        ?: head.name
+}
+
+val tagFrom         = tags.first { versionFromString(it.getString("name").replace("paintera-", "")) < version }
+val commitFrom      = getParentFromTag("saalfeldlab/paintera", tagFrom)
+val relevantCommits = compare(
+        repo    = "saalfeldlab/paintera",
+        shaFrom = commitFrom.getString("sha"),
+        shaTo   = commitTo)
+// relevantCommits.forEach { println(it) }
+val mergeCommits = relevantCommits.filter { it.message.startsWith("Merge pull request #") }
 
 val breaking = mutableListOf<String>()
 val features = mutableListOf<String>()
 val fixes    = mutableListOf<String>()
 
-for (change in mergeCommits) {
-        for (line in change.message.lines()) {
-                if (line.startsWith("[Breaking] ")) {
-                        breaking += line
-                } else if (line.startsWith("[Feature] ")) {
-                        features += line
-                } else if (line.startsWith("[Fixes] ")) {
-                        fixes += line
-                }
+for (commit in mergeCommits) {
+        for (line in commit.message.lines()) {
+                var l = line
+                val isBreaking = if (l.contains("[BREAKING]")) {
+                        l = l.replace("[BREAKING]", "").trim()
+                        true
+                } else false
+                val isFeature = if (l.contains("[FEATURE]")) {
+                        l = l.replace("[FEATURE]", "").trim()
+                        true
+                } else false
+                val isBugfix = if (l.contains("[BUGFIX]")) {
+                        l = l.replace("[BUGFIX]", "").trim()
+                        true
+                } else false
+                if (isBreaking) breaking.add(l)
+                if (isFeature)  features.add(l)
+                if (isBugfix)   fixes.add(l)
         }
 }
 
 println(breaking)
 println(features)
 println(fixes)
+// println("${model.version} is snapshot? $isSnapshot")
+// println("$currentCommit ${getCommitDate("saalfeldlab/paintera", currentCommit)}")
 
