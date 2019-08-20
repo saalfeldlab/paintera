9112f6e7941e9b804681be696a132bcb182c4b34
Igor Pisarev <pisarevi@janelia.hhmi.org>
2019-08-19 20:18:40 -0400

    fix renderer block size test with respect to new limits

--- src/test/java/org/janelia/saalfeldlab/paintera/meshes/RendererBlockSizeTest.java
+++ src/test/java/org/janelia/saalfeldlab/paintera/meshes/RendererBlockSizeTest.java
@@ -33,9 +33,9 @@ public class RendererBlockSizeTest
 		final int[][] blockSizes = MeshGeneratorJobManager.getRendererFullBlockSizes(16, new double[][] {
 				{4, 4, 40}, {8, 8, 40}, {16, 16, 40}, {32, 32, 40}, {64, 64, 80}, {128, 128, 80}
 			});
-		Assert.assertArrayEquals(new int[] {16, 16, 16}, blockSizes[0]);
-		Assert.assertArrayEquals(new int[] {16, 16, 16}, blockSizes[1]);
-		Assert.assertArrayEquals(new int[] {16, 16, 16}, blockSizes[2]);
+		Assert.assertArrayEquals(new int[] {16, 16, 8}, blockSizes[0]);
+		Assert.assertArrayEquals(new int[] {16, 16, 8}, blockSizes[1]);
+		Assert.assertArrayEquals(new int[] {16, 16, 8}, blockSizes[2]);
 		Assert.assertArrayEquals(new int[] {16, 16, 16}, blockSizes[3]);
 		Assert.assertArrayEquals(new int[] {16, 16, 16}, blockSizes[4]);
 		Assert.assertArrayEquals(new int[] {16, 16, 16}, blockSizes[5]);
@@ -47,7 +47,7 @@ public class RendererBlockSizeTest
 		final int[][] blockSizes = MeshGeneratorJobManager.getRendererFullBlockSizes(64, new double[][] {
 				{4, 4, 40}, {8, 8, 40}, {16, 16, 40}, {32, 32, 40}, {64, 64, 80}, {128, 128, 80}
 			});
-		Assert.assertArrayEquals(new int[] {64, 64, 16}, blockSizes[0]);
+		Assert.assertArrayEquals(new int[] {64, 64, 8}, blockSizes[0]);
 		Assert.assertArrayEquals(new int[] {64, 64, 16}, blockSizes[1]);
 		Assert.assertArrayEquals(new int[] {64, 64, 32}, blockSizes[2]);
 		Assert.assertArrayEquals(new int[] {64, 64, 64}, blockSizes[3]);
@@ -75,7 +75,7 @@ public class RendererBlockSizeTest
 		final int[][] blockSizes = MeshGeneratorJobManager.getRendererFullBlockSizes(256, new double[][] {
 				{4, 4, 160}, {8, 8, 160}, {16, 16, 160}, {32, 32, 160}, {64, 64, 160}, {128, 128, 160}, {256, 256, 160}
 			});
-		Assert.assertArrayEquals(new int[] {256, 256, 16}, blockSizes[0]);
+		Assert.assertArrayEquals(new int[] {256, 256, 8}, blockSizes[0]);
 		Assert.assertArrayEquals(new int[] {256, 256, 16}, blockSizes[1]);
 		Assert.assertArrayEquals(new int[] {256, 256, 32}, blockSizes[2]);
 		Assert.assertArrayEquals(new int[] {256, 256, 64}, blockSizes[3]);
