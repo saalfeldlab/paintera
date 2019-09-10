saalfeldlab/paintera

commit 87c98741202b90a6a1a63fdb25999bb525687b9d

Use IoSync shutdown PR to shutdown IoSyncs
Requires imglib/imglib2-cache#12

src/main/java/org/janelia/saalfeldlab/paintera/data/mask/MaskedSource.java
@@ -150,7 +150,7 @@ public class MaskedSource<D extends Type<D>, T extends Type<T>> implements DataS
 
 	private final SharedQueue queue;
 
-	private final CachedCellImg<UnsignedLongType, LongAccess>[] dataCanvases;
+	private final DiskCachedCellImg<UnsignedLongType, LongAccess>[] dataCanvases;
 
 	private final RandomAccessibleInterval<VolatileUnsignedLongType>[] canvases;
 
@@ -244,7 +244,7 @@ public class MaskedSource<D extends Type<D>, T extends Type<T>> implements DataS
 				.mapToObj(level -> Intervals.dimensionsAsLongArray(this.source.getSource(0, level)))
 				.toArray(long[][]::new);
 		this.blockSizes = blockSizes;
-		this.dataCanvases = new CachedCellImg[source.getNumMipmapLevels()];
+		this.dataCanvases = new DiskCachedCellImg[source.getNumMipmapLevels()];
 		this.canvases = new RandomAccessibleInterval[source.getNumMipmapLevels()];
 		this.dMasks = new RealRandomAccessible[this.canvases.length];
 		this.tMasks = new RealRandomAccessible[this.canvases.length];
@@ -263,8 +263,7 @@ public class MaskedSource<D extends Type<D>, T extends Type<T>> implements DataS
 				dataCanvases,
 				canvases,
 				this.dimensions,
-				this.blockSizes
-		));
+				this.blockSizes));
 		this.cacheDirectory.set(initialCacheDirectory);
 
 		this.affectedBlocksByLabel = Stream.generate(HashMap::new).limit(this.canvases.length).toArray(Map[]::new);
@@ -303,24 +302,23 @@ public class MaskedSource<D extends Type<D>, T extends Type<T>> implements DataS
 				LOG.error(
 						"Currently processing, cannot generate new mask: persisting? {} mask in use? {}",
 						isPersisting,
-						currentMask
-				         );
+						currentMask);
 				throw new MaskInUse("Busy, cannot generate new mask.");
 			}
 			this.isCreatingMask = true;
 		}
 		LOG.debug("Generating mask: {}", maskInfo);
 
-		final Pair<RandomAccessibleInterval<UnsignedLongType>, RandomAccessibleInterval<VolatileUnsignedLongType>>
+		final Pair<DiskCachedCellImg<UnsignedLongType, ?>, RandomAccessibleInterval<VolatileUnsignedLongType>>
 				storeWithVolatile = createMaskStoreWithVolatile(maskInfo.level);
-		final RandomAccessibleInterval<UnsignedLongType> store = storeWithVolatile.getKey();
+		final DiskCachedCellImg<UnsignedLongType, ?> store = storeWithVolatile.getKey();
 		final RandomAccessibleInterval<VolatileUnsignedLongType> vstore  = storeWithVolatile.getValue();
 		setMasks(store, vstore, maskInfo.level, maskInfo.value, isPaintedForeground);
 		final AccessedBlocksRandomAccessible<UnsignedLongType> trackingStore = new AccessedBlocksRandomAccessible<>(
 				store,
 				((AbstractCellImg<?,?,?,?>)store).getCellGrid()
 		);
-		final Mask<UnsignedLongType> mask = new Mask<>(maskInfo, trackingStore);
+		final Mask<UnsignedLongType> mask = new Mask<>(maskInfo, trackingStore, store.getCache(), store::shutdown);
 		synchronized (this)
 		{
 			this.currentMask = mask;
@@ -394,7 +392,7 @@ public class MaskedSource<D extends Type<D>, T extends Type<T>> implements DataS
 		synchronized (this)
 		{
 			final RandomAccessibleInterval<UnsignedLongType> rasteredMask = Views.interval(Views.raster(mask), source.getSource(0, maskInfo.level));
-			this.currentMask = new Mask<>(maskInfo, rasteredMask);
+			this.currentMask = new Mask<>(maskInfo, rasteredMask, null, null);
 			this.isCreatingMask = false;
 		}
 	}
@@ -442,6 +440,10 @@ public class MaskedSource<D extends Type<D>, T extends Type<T>> implements DataS
 
 			synchronized (this)
 			{
+				if (this.currentMask.shutdown != null)
+					this.currentMask.shutdown.run();
+				if (this.currentMask.invalidate != null)
+					this.currentMask.invalidate.invalidateAll();
 				this.currentMask = null;
 			}
 
@@ -484,12 +486,10 @@ public class MaskedSource<D extends Type<D>, T extends Type<T>> implements DataS
 		{
 			this.dMasks[level] = ConstantUtils.constantRealRandomAccessible(
 					new UnsignedLongType(Label.INVALID),
-					NUM_DIMENSIONS
-			                                                           );
+					NUM_DIMENSIONS);
 			this.tMasks[level] = ConstantUtils.constantRealRandomAccessible(
 					new VolatileUnsignedLongType(Label.INVALID),
-					NUM_DIMENSIONS
-			                                                           );
+					NUM_DIMENSIONS);
 		}
 	}
 
@@ -1318,7 +1318,7 @@ public class MaskedSource<D extends Type<D>, T extends Type<T>> implements DataS
 
 		private final SharedQueue queue;
 
-		private final CachedCellImg<UnsignedLongType, ?>[] dataCanvases;
+		private final DiskCachedCellImg<UnsignedLongType, ?>[] dataCanvases;
 
 		private final RandomAccessibleInterval<VolatileUnsignedLongType>[] canvases;
 
@@ -1328,7 +1328,7 @@ public class MaskedSource<D extends Type<D>, T extends Type<T>> implements DataS
 
 		public CanvasBaseDirChangeListener(
 				final SharedQueue queue,
-				final CachedCellImg<UnsignedLongType, ?>[] dataCanvases,
+				final DiskCachedCellImg<UnsignedLongType, ?>[] dataCanvases,
 				final RandomAccessibleInterval<VolatileUnsignedLongType>[] canvases,
 				final long[][] dimensions,
 				final int[][] blockSizes)
@@ -1370,11 +1370,13 @@ public class MaskedSource<D extends Type<D>, T extends Type<T>> implements DataS
 							.cellDimensions(blockSizes[level]);
 					final DiskCachedCellImgFactory<UnsignedLongType> f = new DiskCachedCellImgFactory<>(new UnsignedLongType(), o);
 					final CellLoader<UnsignedLongType> loader = img -> img.forEach(t -> t.set(Label.INVALID));
-					final CachedCellImg<UnsignedLongType, ?> store  = f.create(dimensions[level], loader, o);
+					final DiskCachedCellImg<UnsignedLongType, ?> store  = f.create(dimensions[level], loader, o);
 					final RandomAccessibleInterval<VolatileUnsignedLongType> vstore = VolatileViews.wrapAsVolatile(store, queue, new CacheHints(LoadingStrategy.VOLATILE, canvases.length - 1 - level, true));
 
-					if (dataCanvases[level] != null)
+					if (dataCanvases[level] != null) {
+						this.dataCanvases[level].shutdown();
 						this.dataCanvases[level].getCache().invalidateAll();
+					}
 					this.dataCanvases[level] = store;
 					this.canvases[level] = vstore;
 				}
@@ -1477,11 +1479,11 @@ public class MaskedSource<D extends Type<D>, T extends Type<T>> implements DataS
 		return new DiskCachedCellImgFactory<>(new UnsignedLongType(), maskOpts).create(source.getSource(0, level));
 	}
 
-	private Pair<RandomAccessibleInterval<UnsignedLongType>, RandomAccessibleInterval<VolatileUnsignedLongType>> createMaskStoreWithVolatile(final int level)
+	private Pair<DiskCachedCellImg<UnsignedLongType, ?>, RandomAccessibleInterval<VolatileUnsignedLongType>> createMaskStoreWithVolatile(final int level)
 	{
 		final DiskCachedCellImgOptions maskOpts = getMaskDiskCachedCellImgOptions(level);
-		final CachedCellImg<UnsignedLongType, ?> store = createMaskStore(maskOpts, level);
-		final RandomAccessibleInterval<VolatileUnsignedLongType> vstore  = VolatileViews.wrapAsVolatile(store);
+		final DiskCachedCellImg<UnsignedLongType, ?> store = createMaskStore(maskOpts, level);
+		final RandomAccessibleInterval<VolatileUnsignedLongType> vstore  = VolatileViews.wrapAsVolatile(store, queue, new CacheHints(LoadingStrategy.VOLATILE, 0, true));
 		return new Pair<>(store, vstore);
 	}
 

pom.xml
@@ -35,7 +35,7 @@
 		<!-- NB: Deploy releases to the SciJava Maven repository. -->
 		<releaseProfiles>deploy-to-scijava</releaseProfiles>
 
-		<imglib2-cache.version>1.0.0-beta-12</imglib2-cache.version>
+		<imglib2-cache.version>1.0.0-beta-13-SNAPSHOT</imglib2-cache.version>
 		<bigdataviewer-vistools.version>1.0.0-beta-18-SNAPSHOT</bigdataviewer-vistools.version>
 		<picocli.version>4.0.1</picocli.version>
 		<kotlin.compiler.jvmTarget>1.8</kotlin.compiler.jvmTarget>

src/main/java/org/janelia/saalfeldlab/paintera/data/mask/Mask.java
@@ -1,6 +1,7 @@
 package org.janelia.saalfeldlab.paintera.data.mask;
 
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.cache.Invalidate;
 
 public class Mask<D> {
 
@@ -8,8 +9,18 @@ public class Mask<D> {
 
 	public final RandomAccessibleInterval<D> mask;
 
-	public Mask(MaskInfo<D> info, RandomAccessibleInterval<D> mask) {
+	public final Invalidate<?> invalidate;
+
+	public final Runnable shutdown;
+
+	public Mask(
+			final MaskInfo<D> info,
+			final RandomAccessibleInterval<D> mask,
+			final Invalidate<?> invalidate,
+			final Runnable shutdown) {
 		this.info = info;
 		this.mask = mask;
+		this.invalidate = invalidate;
+		this.shutdown = shutdown;
 	}
 }

commit 0fe5da9c40b1fcd38f67bc204e52912dcab7fd88

Release mask resources only after propagation
Fixes issues introduced in 87c98741202b90a6a1a63fdb25999bb525687b9d and brought up in #316:
 - https://github.com/saalfeldlab/paintera/pull/316#issuecomment-530116538
 - https://github.com/saalfeldlab/paintera/commit/87c98741202b90a6a1a63fdb25999bb525687b9d#diff-5b950cab32344141b1cccad3c48c5381R443-R446

src/main/java/org/janelia/saalfeldlab/paintera/data/mask/MaskedSource.java
@@ -471,14 +471,9 @@ public class MaskedSource<D extends Type<D>, T extends Type<T>> implements DataS
 					canvas.getCellGrid(),
 					paintedInterval);
 
+			final Mask<UnsignedLongType> currentMaskBeforePropagation = this.currentMask;
 			synchronized (this)
 			{
-				if (this.currentMask.shutdown != null)
-					this.currentMask.shutdown.run();
-				if (this.currentMask.invalidate != null)
-					this.currentMask.invalidate.invalidateAll();
-				if (this.currentMask.invalidateVolatile != null)
-					this.currentMask.invalidateVolatile.invalidateAll();
 				this.currentMask = null;
 			}
 
@@ -495,19 +490,29 @@ public class MaskedSource<D extends Type<D>, T extends Type<T>> implements DataS
 			this.affectedBlocks.addAll(paintedBlocksAtHighestResolution);
 
 			propagationExecutor.submit(() -> {
-				propagateMask(
-						mask.mask,
-						affectedBlocks,
-						maskInfo.level,
-						maskInfo.value,
-						paintedInterval,
-						acceptAsPainted
-					);
-				setMasksConstant();
-				synchronized (this)
-				{
-					LOG.debug("Done applying mask!");
-					this.isApplyingMask.set(false);
+				try {
+					propagateMask(
+							mask.mask,
+							affectedBlocks,
+							maskInfo.level,
+							maskInfo.value,
+							paintedInterval,
+							acceptAsPainted);
+					setMasksConstant();
+					synchronized (this) {
+						LOG.debug("Done applying mask!");
+						this.isApplyingMask.set(false);
+					}
+				} finally {
+					// free resources
+					if (currentMaskBeforePropagation != null) {
+						if (currentMaskBeforePropagation.shutdown != null)
+							currentMaskBeforePropagation.shutdown.run();
+						if (currentMaskBeforePropagation.invalidate != null)
+							currentMaskBeforePropagation.invalidate.invalidateAll();
+						if (currentMaskBeforePropagation.invalidateVolatile != null)
+							currentMaskBeforePropagation.invalidateVolatile.invalidateAll();
+					}
 				}
 			});
 