16333672e48f53812e69bbd870efea979ab3b48d
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2020-03-23 16:08:45 -0400

    Remove slf4j endpoint and update version in generate-bash-completion.kts

--- generate-bash-completion.kts
+++ generate-bash-completion.kts
@@ -2,8 +2,7 @@
 
 @file:MavenRepository("scijava.public", "https://maven.scijava.org/content/groups/public")
 @file:KotlinOpts("-classpath /usr/lib/jvm/java-8-openjdk/jre/lib/ext/jfxrt.jar")
-@file:DependsOn("org.janelia.saalfeldlab:paintera:0.23.2-SNAPSHOT")
-@file:DependsOn("org.slf4j:slf4j-simple:1.7.25")
+@file:DependsOn("org.janelia.saalfeldlab:paintera:0.24.1-SNAPSHOT")
 
 import picocli.AutoComplete
 
