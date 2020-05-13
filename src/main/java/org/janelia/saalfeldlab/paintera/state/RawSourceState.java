a5b956e42c3a06d0d5a3ce8f5840d607d31ef60d
Igor Pisarev <pisarevi@janelia.hhmi.org>
2020-05-13 11:41:20 -0400

    Remove Ctrl+T shortcut in favor of the new menu that will be added

--- src/main/java/org/janelia/saalfeldlab/paintera/state/RawSourceState.java
+++ src/main/java/org/janelia/saalfeldlab/paintera/state/RawSourceState.java
@@ -1,13 +1,7 @@
 package org.janelia.saalfeldlab.paintera.state;
 
 import bdv.util.volatiles.VolatileTypeMatcher;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.scene.Node;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCodeCombination;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.cache.Invalidate;
@@ -21,13 +15,9 @@ import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.volatiles.AbstractVolatileNativeRealType;
 import net.imglib2.util.Util;
 import net.imglib2.view.Views;
-import org.janelia.saalfeldlab.fx.event.DelegateEventHandlers;
-import org.janelia.saalfeldlab.fx.event.KeyTracker;
-import org.janelia.saalfeldlab.paintera.NamedKeyCombination;
 import org.janelia.saalfeldlab.paintera.PainteraBaseView;
 import org.janelia.saalfeldlab.paintera.composition.Composite;
 import org.janelia.saalfeldlab.paintera.composition.CompositeCopy;
-import org.janelia.saalfeldlab.paintera.config.input.KeyAndMouseBindings;
 import org.janelia.saalfeldlab.paintera.data.DataSource;
 import org.janelia.saalfeldlab.paintera.data.RandomAccessibleIntervalDataSource;
 import org.slf4j.Logger;
@@ -45,20 +35,15 @@ public class RawSourceState<D, T extends RealType<T>>
 	private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
 	private static Invalidate<Long> NO_OP_INVALIDATE = new Invalidate<Long>() {
-		@Override
-		public void invalidate(Long key) {
-
-		}
 
 		@Override
-		public void invalidateIf(long parallelismThreshold, Predicate<Long> condition) {
-
-		}
+		public void invalidate(Long key) {}
 
 		@Override
-		public void invalidateAll(long parallelismThreshold) {
+		public void invalidateIf(long parallelismThreshold, Predicate<Long> condition) {}
 
-		}
+		@Override
+		public void invalidateAll(long parallelismThreshold) {}
 	};
 
 	public RawSourceState(
@@ -137,35 +122,10 @@ public class RawSourceState<D, T extends RealType<T>>
 	}
 
 	@Override
-	public EventHandler<Event> stateSpecificGlobalEventHandler(PainteraBaseView paintera, KeyTracker keyTracker) {
-		final KeyAndMouseBindings bindings = paintera.getKeyAndMouseBindings().getConfigFor(this);
-		LOG.debug("Returning {}-specific global handler", getClass().getSimpleName());
-		final DelegateEventHandlers.AnyHandler handler = DelegateEventHandlers.handleAny();
-		final EventHandler<KeyEvent> threshold = new RawSourceStateThreshold(this).keyPressedHandler(paintera, bindings.getKeyCombinations().get(BindingKeys.THRESHOLD)::getPrimaryCombination);
-		handler.addEventHandler(KeyEvent.KEY_PRESSED, threshold);
-		return handler;
-	}
-
-	@Override
 	public Node preferencePaneNode() {
 		final Node node = super.preferencePaneNode();
 		final VBox box = node instanceof VBox ? (VBox) node : new VBox(node);
 		box.getChildren().add(new RawSourceStateConverterNode(this.converter()).getConverterNode());
 		return box;
-	}
-
-	@Override
-	public KeyAndMouseBindings createKeyAndMouseBindings() {
-		final KeyAndMouseBindings bindings = new KeyAndMouseBindings();
-		try {
-			bindings.getKeyCombinations().addCombination(new NamedKeyCombination(BindingKeys.THRESHOLD, new KeyCodeCombination(KeyCode.T, KeyCombination.CONTROL_DOWN)));
-		} catch (final NamedKeyCombination.CombinationMap.KeyCombinationAlreadyInserted e) {
-			// TOOD no reason to ever throw anything with only a single inserted combination
-		}
-		return bindings;
-	}
-
-	private static class BindingKeys {
-		private static final String THRESHOLD = "threshold";
 	}
 }
