0bb3e2435b15ee073397604d138b95331c6f92cf
Caleb Hulbert <cmhulbert@gmail.com>
2021-05-03 14:49:17 -0400

    debug too verbso -> trace

--- src/main/kotlin/org/janelia/saalfeldlab/paintera/PainteraDefaultHandlers.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/paintera/PainteraDefaultHandlers.kt
@@ -19,7 +19,12 @@ import javafx.event.Event
 import javafx.event.EventHandler
 import javafx.scene.Node
 import javafx.scene.control.ContextMenu
-import javafx.scene.input.*
+import javafx.scene.input.KeyCode
+import javafx.scene.input.KeyCodeCombination
+import javafx.scene.input.KeyCombination
+import javafx.scene.input.KeyEvent
+import javafx.scene.input.MouseButton
+import javafx.scene.input.MouseEvent
 import javafx.scene.transform.Affine
 import net.imglib2.FinalRealInterval
 import net.imglib2.Interval
@@ -40,7 +45,11 @@ import org.janelia.saalfeldlab.fx.ortho.OrthogonalViews.ViewerAndTransforms
 import org.janelia.saalfeldlab.fx.ui.Exceptions
 import org.janelia.saalfeldlab.paintera.config.BookmarkConfig
 import org.janelia.saalfeldlab.paintera.config.BookmarkSelectionDialog
-import org.janelia.saalfeldlab.paintera.control.*
+import org.janelia.saalfeldlab.paintera.control.FitToInterval
+import org.janelia.saalfeldlab.paintera.control.Navigation
+import org.janelia.saalfeldlab.paintera.control.OrthoViewCoordinateDisplayListener
+import org.janelia.saalfeldlab.paintera.control.OrthogonalViewsValueDisplayListener
+import org.janelia.saalfeldlab.paintera.control.RunWhenFirstElementIsAdded
 import org.janelia.saalfeldlab.paintera.control.actions.MenuActionType
 import org.janelia.saalfeldlab.paintera.control.actions.NavigationActionType
 import org.janelia.saalfeldlab.paintera.control.navigation.DisplayTransformUpdateOnResize
