670ababa6bd71787144925040d1ac2889cc934d9
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2021-08-23 22:12:09 -0400

    fix: fetch specific ci-setup-github-actions.sh commit

--- .github/setup.sh
+++ .github/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/ci-setup-github-actions.sh
+curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/45cc7f512de62b9b9e285264eb628ce4b5e516a7/ci-setup-github-actions.sh
 sh ci-setup-github-actions.sh
