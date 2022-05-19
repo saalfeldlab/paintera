f4031e2289bb573b8eba7b1cdc1b1809e0822063
Caleb Hulbert <cmhulbert@users.noreply.github.com>
2022-05-19 11:59:10 -0400

    refactor!: use read only properties

--- src/main/java/org/janelia/saalfeldlab/paintera/meshes/SegmentMeshInfos.java
+++ src/main/java/org/janelia/saalfeldlab/paintera/meshes/SegmentMeshInfos.java
@@ -1,20 +1,20 @@
 package org.janelia.saalfeldlab.paintera.meshes;
 
 import javafx.beans.InvalidationListener;
+import javafx.beans.property.ReadOnlyListProperty;
+import javafx.beans.property.ReadOnlyListWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import org.janelia.saalfeldlab.paintera.control.selection.SelectedSegments;
 import org.janelia.saalfeldlab.paintera.meshes.managed.MeshManagerWithAssignmentForSegments;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class SegmentMeshInfos {
 
-  private final ObservableList<SegmentMeshInfo> infos = FXCollections.observableArrayList();
-
-  private final ObservableList<SegmentMeshInfo> readOnlyInfos = FXCollections.unmodifiableObservableList(infos);
+  private final ReadOnlyListWrapper<SegmentMeshInfo> readOnlyInfos = new ReadOnlyListWrapper<>(FXCollections.observableArrayList());
 
   private final ManagedMeshSettings meshSettings;
 
@@ -33,28 +33,35 @@ public class SegmentMeshInfos {
 
 	final InvalidationListener updateMeshInfosHandler = obs -> {
 	  final long[] segments = selectedSegments.getSelectedSegmentsCopyAsArray();
-	  final List<SegmentMeshInfo> infos = Arrays
-			  .stream(segments)
-			  .mapToObj(id -> {
-				final MeshSettings settings = meshSettings.getOrAddMesh(id, true);
-				return new SegmentMeshInfo(
-						id,
-						settings,
-						meshSettings.isManagedProperty(id),
-						selectedSegments.getAssignment(),
-						meshManager);
-			  })
-			  .collect(Collectors.toList());
-	  this.infos.setAll(infos);
+	  final var curInfos = this.infos().stream().map(SegmentMeshInfo::segmentId).collect(Collectors.toSet());
+	  this.infos().removeIf(info -> !selectedSegments.isSegmentSelected(info.segmentId()));
+	  final var addInfos = new ArrayList<SegmentMeshInfo>();
+	  Arrays.stream(segments).forEach(id -> {
+		if (!curInfos.contains(id)) {
+		  final MeshSettings settings = meshSettings.getOrAddMesh(id, true);
+		  addInfos.add(new SegmentMeshInfo(
+				  id,
+				  settings,
+				  meshSettings.isManagedProperty(id),
+				  selectedSegments.getAssignment(),
+				  meshManager));
+		}
+	  });
+	  this.infos().addAll(addInfos);
 	};
 
 	meshManager.getMeshUpdateObservable().addListener(updateMeshInfosHandler);
 	meshSettings.isMeshListEnabledProperty().addListener(updateMeshInfosHandler);
   }
 
-  public ObservableList<SegmentMeshInfo> readOnlyInfos() {
+  private ObservableList<SegmentMeshInfo> infos() {
 
-	return this.readOnlyInfos;
+	return readOnlyInfos.get();
+  }
+
+  public ReadOnlyListProperty<SegmentMeshInfo> readOnlyInfos() {
+
+	return this.readOnlyInfos.getReadOnlyProperty();
   }
 
   public ManagedMeshSettings meshSettings() {
