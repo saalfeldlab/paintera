afb8079808c54f3cd8b4506d754a233fabf3e9da
Caleb Hulbert <cmhulbert@gmail.com>
2023-03-24 14:42:05 -0400

    fix: improve device cleanup (partial solution for #479)

--- src/main/kotlin/org/janelia/saalfeldlab/paintera/DeviceManager.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/paintera/DeviceManager.kt
@@ -2,14 +2,42 @@ package org.janelia.saalfeldlab.paintera
 
 import org.janelia.saalfeldlab.control.mcu.MCUControlPanel
 import org.janelia.saalfeldlab.control.mcu.XTouchMiniMCUControlPanel
+import org.slf4j.LoggerFactory
+import java.lang.invoke.MethodHandles
+import javax.sound.midi.MidiUnavailableException
 
 object DeviceManager {
 
-	val xTouchMini: MCUControlPanel? by lazy {
-		try {
-			XTouchMiniMCUControlPanel.build()
-		} catch (e: Exception) {
+	private val LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
+
+	private val activeMidiDevices = mutableListOf<MCUControlPanel>()
+
+	var xTouchMini: MCUControlPanel? = loadXTouchMini()
+		private set
+
+	private fun loadXTouchMini() = loadMidiDevice { XTouchMiniMCUControlPanel.build().also { activeMidiDevices += it } }
+
+	private fun loadMidiDevice(loadDevice: () -> MCUControlPanel?): MCUControlPanel? {
+		return try {
+			loadDevice()
+		} catch (_: MidiUnavailableException) {
 			null
 		}
+	}
+
+	fun closeMidiDevices() {
+		activeMidiDevices.removeIf {
+			try {
+				it.close()
+				true
+			} catch (e: Exception) {
+				LOG.error("Unable to close device $it")
+				false
+			}
+		}
+	}
+
+	fun closeDevices() {
+		closeMidiDevices()
 	}
 }
