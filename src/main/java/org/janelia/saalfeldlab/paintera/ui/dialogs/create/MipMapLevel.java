158d24a213b898c836f0bfda993a901d7d575d26
Caleb Hulbert <cmhulbert@gmail.com>
2023-07-10 09:40:58 -0400

    feat: improve create dataset dialog

--- src/main/java/org/janelia/saalfeldlab/paintera/ui/dialogs/create/MipMapLevel.java
+++ src/main/java/org/janelia/saalfeldlab/paintera/ui/dialogs/create/MipMapLevel.java
@@ -1,10 +1,14 @@
 package org.janelia.saalfeldlab.paintera.ui.dialogs.create;
 
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
@@ -27,6 +31,8 @@ public class MipMapLevel {
 
 	final Node node;
 
+	final SimpleBooleanProperty showHeader = new SimpleBooleanProperty(false);
+
 	public MipMapLevel(int downsamplingFactor, int maxNumEntries, double fieldWidth, final double nameWidth, ObjectField.SubmitOn... submitOn) {
 
 		this(
@@ -46,10 +52,30 @@ public class MipMapLevel {
 		this.maxNumberOfEntriesPerSet = maxNumberOfEntriesPerSet;
 		this.fieldWidth = fieldWidth;
 
-		this.node = new HBox(
-				NamedNode.nameIt("Relative Factors", nameWidth, false, relativeDownsamplingFactors.getNode()),
-				NamedNode.nameIt("Max Num Entries", nameWidth, false, maxNumberOfEntriesPerSet.getTextField())
+		final HBox relativeFactorsHeader = createHeader("Relative Factors");
+		final HBox maxEntriesHeader = createHeader("Max Entries");
+
+		final HBox mipMapRow = new HBox(
+				new VBox(relativeFactorsHeader, relativeDownsamplingFactors.getNode()),
+				new VBox(maxEntriesHeader, maxNumberOfEntriesPerSet.getTextField())
 		);
+
+
+		mipMapRow.setPadding(new Insets(0, 10.0, 0, 10.0));
+		mipMapRow.spacingProperty().setValue(10.0);
+		this.node = mipMapRow;
+
+	}
+
+	private HBox createHeader(String headerText) {
+		final Label label = new Label(headerText);
+		final HBox header = new HBox(label);
+		header.setAlignment(Pos.BOTTOM_CENTER);
+		HBox.setHgrow(label, Priority.ALWAYS);
+		header.setPadding(new Insets(0, 0, 3.0, 0));
+		header.visibleProperty().bind(showHeader);
+		header.managedProperty().bind(showHeader);
+		return header;
 	}
 
 	public double[] downsamplingFactors() {
@@ -74,20 +100,23 @@ public class MipMapLevel {
 		levels.addListener((ListChangeListener<MipMapLevel>)change -> InvokeOnJavaFXApplicationThread.invoke(() -> {
 			children.stream().filter(n -> n instanceof Pane).map(n -> (Pane)n).map(Pane::getChildren).forEach(List::clear);
 			children.clear();
-			for (MipMapLevel l : levels) {
+			for (int i = 0; i < levels.size(); i++) {
+				final MipMapLevel level = levels.get(i);
 				final Button removeButton = new Button("-");
 				removeButton.setPrefWidth(buttonWidth);
 				removeButton.setMinWidth(buttonWidth);
 				removeButton.setMaxWidth(buttonWidth);
 				removeButton.setOnAction(e -> {
 					e.consume();
-					levels.remove(l);
+					levels.remove(level);
 				});
 				final Node filler = NamedNode.bufferNode();
 				filler.minWidth(10);
+				final Node filler2 = NamedNode.bufferNode();
+				filler2.minWidth(10);
 				HBox.setHgrow(filler, Priority.ALWAYS);
-				final HBox b = new HBox(l.node, filler, removeButton);
-				children.add(b);
+				final HBox scaleLevelRow = new HBox(new Label("Scale " + i + ": "), level.node, removeButton);
+				children.add(scaleLevelRow);
 			}
 		}));
 
@@ -95,13 +124,11 @@ public class MipMapLevel {
 		addButton.setPrefWidth(buttonWidth);
 		addButton.setMinWidth(buttonWidth);
 		addButton.setMaxWidth(buttonWidth);
-		final Node filler = NamedNode.bufferNode();
-		final HBox addBox = new HBox(filler, addButton);
 		addButton.setOnAction(e -> {
 			e.consume();
 			levels.add(new MipMapLevel(2, -1, fieldWidth, nameWidth, submitOn));
 		});
 
-		return new VBox(addBox, levelsBox);
+		return new VBox(addButton, levelsBox);
 	}
 }
