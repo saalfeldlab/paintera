7737784263f659a66bcc74fff30b3d128bdcd7cd
Caleb Hulbert <cmhulbert@gmail.com>
2025-01-21 15:01:42 -0500

    test(wip): AnimatedProgressBarTest

---
+++ src/test/kotlin/org/janelia/saalfeldlab/paintera/ui/dialogs/AnimatedProgressBarTest.kt
@@ -0,0 +1,36 @@
+package org.janelia.saalfeldlab.paintera.ui.dialogs
+
+import javafx.beans.property.SimpleDoubleProperty
+import javafx.beans.property.SimpleStringProperty
+import kotlinx.coroutines.delay
+import org.janelia.saalfeldlab.fx.util.InvokeOnJavaFXApplicationThread
+import kotlin.test.Test
+
+class AnimatedProgressBarTest {
+
+	@Test
+	fun test() {
+		val progressBinding = SimpleDoubleProperty(.1)
+		InvokeOnJavaFXApplicationThread {
+			val progressStringBinding = SimpleStringProperty("Another test " )
+			val dialog = AnimatedProgressBarAlert("test", "this is a test", progressStringBinding, progressBinding)
+			dialog.showAndStart()
+
+		}
+		InvokeOnJavaFXApplicationThread {
+			progressBinding.value = .75
+			progressBinding.value = 1.0
+			progressBinding.value = 1.5
+		}
+
+		Thread.sleep(100000)
+	}
+
+
+}
+
+fun main() {
+	InvokeOnJavaFXApplicationThread {
+
+	}
+}
