117d7fc6de953652d9b1f92990eac08bdc032e1e
Caleb Hulbert <cmhulbert@gmail.com>
2025-04-25 15:49:55 -0400

    feat: help glyphs

--- src/main/kotlin/org/janelia/saalfeldlab/paintera/Style.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/paintera/Style.kt
@@ -2,6 +2,7 @@ package org.janelia.saalfeldlab.paintera
 
 object Style {
 
+	val HELP_GLYPH = arrayOf("glyph-icon", "help")
 	val ADD_GLYPH = arrayOf("glyph-icon", "add")
 	val REMOVE_GLYPH = arrayOf("glyph-icon", "remove")
 }
