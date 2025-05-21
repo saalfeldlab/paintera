3bb04e781d4474b712892a95abb643b7106a8b85
Caleb Hulbert <cmhulbert@gmail.com>
2025-05-20 21:47:09 -0400

    chore: swap varible names to match type

--- src/main/java/org/janelia/saalfeldlab/paintera/state/SourceInfo.java
+++ src/main/java/org/janelia/saalfeldlab/paintera/state/SourceInfo.java
@@ -87,8 +87,8 @@ public class SourceInfo {
 	private final IntegerProperty currentSourceIndex = new SimpleIntegerProperty(-1);
 
 	{
-		this.currentSource.addListener((oldv, obs, newv) -> this.currentSourceIndex.set(this.sources.indexOf(newv)));
-		this.currentSourceIndex.addListener((oldv, obs, newv) -> this.currentSource.set(newv.intValue() < 0 || newv
+		this.currentSource.addListener((obs, oldv, newv) -> this.currentSourceIndex.set(this.sources.indexOf(newv)));
+		this.currentSourceIndex.addListener((obs, oldv, newv) -> this.currentSource.set(newv.intValue() < 0 || newv
 				.intValue() >= this.sources.size()
 				? null
 				: this.sources.get(newv
