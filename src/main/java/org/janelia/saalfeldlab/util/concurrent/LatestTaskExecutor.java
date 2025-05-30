4b67a8354392d79cf8596b62322f639b5fc9f9e5
Caleb Hulbert <cmhulbert@gmail.com>
2025-05-30 11:23:37 -0400

    revert: LatestTaskExecutor changes

--- src/main/java/org/janelia/saalfeldlab/util/concurrent/LatestTaskExecutor.java
+++ src/main/java/org/janelia/saalfeldlab/util/concurrent/LatestTaskExecutor.java
@@ -2,41 +2,58 @@ package org.janelia.saalfeldlab.util.concurrent;
 
 import java.util.List;
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 
 public class LatestTaskExecutor implements Executor {
 
-	private final ExecutorService executor;
+	private final ScheduledExecutorService executor;
 
-	private Future<?> task;
+	private Runnable task;
+
+	private long delayInNanoSeconds;
+
+	private long lastTaskExecutionNanoTime;
 
 	public LatestTaskExecutor(final ThreadFactory factory) {
 
+		this(0, factory);
+	}
+
+	public LatestTaskExecutor(final long delayInNanoSeconds, final ThreadFactory factory) {
+
 		super();
-		this.executor = Executors.newSingleThreadExecutor(factory);
+		this.executor = Executors.newSingleThreadScheduledExecutor(factory);
+		this.delayInNanoSeconds = delayInNanoSeconds;
 	}
 
 	@Override
 	public synchronized void execute(final Runnable command) {
-		submit(command);
-	}
 
-	public synchronized Future<?> submit(final Runnable command) {
-		if (task != null) {
-			/* cancel if it hasn't started, let it run if not */
-			task.cancel(false);
+		final Runnable pendingTask = task;
+		task = command;
+		if (pendingTask == null) {
+			executor.schedule(
+					() -> {
+						final Runnable currentTask;
+						synchronized (this) {
+							lastTaskExecutionNanoTime = System.nanoTime();
+							currentTask = task;
+							task = null;
+						}
+						currentTask.run();
+					},
+					Math.max(delayInNanoSeconds - (System.nanoTime() - lastTaskExecutionNanoTime), 0),
+					TimeUnit.NANOSECONDS
+			);
 		}
-		final Future<?> future = executor.submit(command);
-		task = future;
-		return future;
 	}
 
 	public synchronized boolean busy() {
 
-		return task == null || !(task.isDone() || task.isCancelled()) ;
+		return task == null;
 	}
 
 	public synchronized void shutDown() {
@@ -49,9 +66,8 @@ public class LatestTaskExecutor implements Executor {
 		return this.executor.shutdownNow();
 	}
 
-	@Deprecated
 	public synchronized void setDelay(final long delayInNanoSeconds) {
 
-		//no-op; class should be removed in the future
+		this.delayInNanoSeconds = delayInNanoSeconds;
 	}
 }
