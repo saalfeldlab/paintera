822166fe9c8871a7f4298a62d362dae486845e48
Caleb Hulbert <cmhulbert@gmail.com>
2025-12-16 09:34:55 -0500

    fix: bug with not updating invalid pixels (sometimes).
    refactor:  VolatileHierarchyProjectorPreMultiply is now just has a PreMultiple wrapped converter

--- src/main/java/org/janelia/saalfeldlab/bdv/fx/viewer/project/VolatileHierarchyProjectorPreMultiply.java
+++ src/main/java/org/janelia/saalfeldlab/bdv/fx/viewer/project/VolatileHierarchyProjectorPreMultiply.java
@@ -5,15 +5,11 @@ import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.Volatile;
 import net.imglib2.converter.Converter;
-import net.imglib2.loops.LoopBuilder;
 import net.imglib2.parallel.TaskExecutor;
 import net.imglib2.type.numeric.ARGBType;
 import net.imglib2.type.numeric.integer.ByteType;
-import org.janelia.saalfeldlab.net.imglib2.view.BundleView;
-import net.imglib2.view.Views;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class VolatileHierarchyProjectorPreMultiply<A extends Volatile<?>> extends VolatileHierarchyProjector<A, ARGBType> {
 
@@ -24,45 +20,13 @@ public class VolatileHierarchyProjectorPreMultiply<A extends Volatile<?>> extend
 			final RandomAccessibleInterval<ByteType> mask,
 			final TaskExecutor taskExecutor) {
 
-		super(sources, converter, target, mask, taskExecutor);
+		super(sources, getPremultiplyConverter(converter), target, mask, taskExecutor);
 	}
 
-	@Override
-	protected void map(byte resolutionIndex, int startHeight, int endHeight) {
-
-
-		if (canceled.get())
-			return;
-
-		final AtomicInteger myNumInvalidPixels = new AtomicInteger();
-
-		LoopBuilder.setImages(
-						Views.interval(new BundleView<>(target), sourceInterval),
-						Views.interval(new BundleView<>(sources.get(resolutionIndex)), sourceInterval),
-						Views.interval(mask, sourceInterval)
-				).multiThreaded(taskExecutor)
-				.forEachChunk(chunk -> {
-					if (canceled.get()) {
-						Thread.currentThread().interrupt();
-						return null;
-					}
-					chunk.forEachPixel((targetVal, sourceVal, maskVal) -> {
-						if (maskVal.get() > resolutionIndex) {
-							final A a = sourceVal.get();
-							final boolean v = a.isValid();
-							if (v) {
-								final ARGBType argb = targetVal.get();
-								converter.convert(a, argb);
-								argb.set(PixelUtils.NonPretoPre(argb.get()));
-								maskVal.set(resolutionIndex);
-							} else
-								myNumInvalidPixels.incrementAndGet();
-						}
-					});
-					return null;
-				});
-		numInvalidPixels.addAndGet(myNumInvalidPixels.get());
-		if (myNumInvalidPixels.get() != 0)
-			valid = false;
+	private static <A> Converter<A, ARGBType> getPremultiplyConverter(Converter<A, ARGBType> converter) {
+		return (source, target) -> {
+			converter.convert(source, target);
+			target.set(PixelUtils.NonPretoPre(target.get()));
+		};
 	}
 }
