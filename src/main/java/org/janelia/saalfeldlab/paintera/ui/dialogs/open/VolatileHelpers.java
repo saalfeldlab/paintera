3d9ff12c71db6e47a507f7ba82581056df4be404
Caleb Hulbert <cmhulbert@gmail.com>
2026-02-19 10:16:13 -0500

    fix: revert EMPTY_ACCESS special case, it has some bugs that need to be more thought out if we want to implement something like this.

--- src/main/java/org/janelia/saalfeldlab/paintera/ui/dialogs/open/VolatileHelpers.java
+++ src/main/java/org/janelia/saalfeldlab/paintera/ui/dialogs/open/VolatileHelpers.java
@@ -5,10 +5,12 @@ import net.imglib2.img.cell.Cell;
 import net.imglib2.img.cell.CellGrid;
 import net.imglib2.type.label.Label;
 import net.imglib2.type.label.VolatileLabelMultisetArray;
+import net.imglib2.util.Intervals;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
+import java.util.Arrays;
 
 public class VolatileHelpers {
 
@@ -34,10 +36,15 @@ public class VolatileHelpers {
 			final int[] cellDims = new int[cellPosition.length];
 			grid.getCellDimensions(cellPosition, cellMin, cellDims);
 
-			return new Cell<>(cellDims, cellMin, EMPTY_ACCESS);
+			return new Cell<>(cellDims, cellMin, newEmptyAccess((int)Intervals.numElements(cellDims), false));
 		}
 	}
 
-	private static final VolatileLabelMultisetArray EMPTY_ACCESS = new VolatileLabelMultisetArray(0, false, new long[]{Label.INVALID});
+	public static VolatileLabelMultisetArray newEmptyAccess(int numElements, boolean isValid) {
+
+		long[] argMaxes = new long[numElements];
+		Arrays.fill(argMaxes, Label.INVALID);
+		return new VolatileLabelMultisetArray(numElements, isValid, argMaxes);
+	}
 
 }
